XanClic/imago

commit 196b7e09619758ab305f122bc3915c6757084316

Fix alignment at the end of file and post-EOF read
Aligning to the end of file is sufficient, we do not need to align
further.

Also, fix post-EOF reads; they should return 0 and be properly handled
by storage drivers.

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/file.rs
@@ -16,6 +16,7 @@ use std::os::windows::fs::{FileExt, OpenOptionsExt};
 #[cfg(windows)]
 use std::os::windows::io::AsRawHandle;
 use std::path::PathBuf;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::RwLock;
 #[cfg(windows)]
 use windows_sys::Win32::System::Ioctl::{FILE_ZERO_DATA_INFORMATION, FSCTL_SET_ZERO_DATA};
@@ -33,16 +34,26 @@ pub struct File {
 
     /// For debug purposes.
     filename: PathBuf,
+
+    /// Cached file length.
+    ///
+    /// Third parties changing the length concurrently is pretty certain to break things anyway.
+    size: AtomicU64,
 }
 
-impl From<fs::File> for File {
-    fn from(file: fs::File) -> Self {
-        File {
+impl TryFrom<fs::File> for File {
+    type Error = io::Error;
+
+    fn try_from(mut file: fs::File) -> io::Result<Self> {
+        let size = file.seek(SeekFrom::End(0))?;
+
+        Ok(File {
             file: RwLock::new(file),
             // TODO: Find out, or better yet, drop `direct_io` and just probe the alignment.
             direct_io: false,
             filename: PathBuf::new(),
-        }
+            size: AtomicU64::new(size),
+        })
     }
 }
 
@@ -65,7 +76,9 @@ impl Storage for File {
         }
 
         let filename_owned = filename.to_owned();
-        let file = file_opts.open(filename)?;
+        let mut file = file_opts.open(filename)?;
+
+        let size = file.seek(SeekFrom::End(0))?;
 
         #[cfg(target_os = "macos")]
         if opts.direct {
@@ -84,6 +97,7 @@ impl Storage for File {
             file: RwLock::new(file),
             direct_io: opts.direct,
             filename: filename_owned,
+            size: AtomicU64::new(size),
         })
     }
 
@@ -106,12 +120,27 @@ impl Storage for File {
     }
 
     fn size(&self) -> io::Result<u64> {
-        let mut file = self.file.write().unwrap();
-        file.seek(SeekFrom::End(0))
+        Ok(self.size.load(Ordering::Relaxed))
     }
 
     #[cfg(unix)]
     async fn readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
+        let sz = self.size.load(Ordering::Relaxed);
+        let end = offset
+            .checked_add(bufv.len())
+            .ok_or_else(|| io::Error::other("Read offset overflow"))?;
+        let bufv = if end > sz {
+            let (head, mut tail) = bufv.split_at(sz - offset);
+            tail.fill(0);
+            head
+        } else {
+            bufv
+        };
+
+        if bufv.is_empty() {
+            return Ok(());
+        }
+
         // TODO: Use `read_vectored_at()` once `unix_file_vectored_at` is stable
         for mut buffer in bufv.into_inner() {
             let next_offset = offset
@@ -131,7 +160,12 @@ impl Storage for File {
         for mut buffer in bufv.into_inner() {
             let mut buffer: &mut [u8] = &mut buffer;
             while !buffer.is_empty() {
-                let len = self.file.write().unwrap().seek_read(buffer, offset)?;
+                let len = if offset >= self.size.load(Ordering::Relaxed) {
+                    buffer.fill(0);
+                    buffer.len()
+                } else {
+                    self.file.write().unwrap().seek_read(buffer, offset)?
+                };
                 offset = offset
                     .checked_add(len as u64)
                     .ok_or_else(|| io::Error::other("Read offset overflow"))?;
@@ -150,6 +184,7 @@ impl Storage for File {
                 .ok_or_else(|| io::Error::other("Write offset overflow"))?;
             self.file.read().unwrap().write_all_at(&buffer, offset)?;
             offset = next_offset;
+            self.size.fetch_max(offset, Ordering::Relaxed);
         }
         Ok(())
     }
@@ -163,6 +198,7 @@ impl Storage for File {
                 offset = offset
                     .checked_add(len as u64)
                     .ok_or_else(|| io::Error::other("Write offset overflow"))?;
+                self.size.fetch_max(offset, Ordering::Relaxed);
                 buffer = buffer.split_at(len).1;
             }
         }
@@ -291,12 +327,11 @@ impl File {
     /// If the range is not at the end of the file, i.e. another method of discarding is needed,
     /// return `false`.
     fn try_discard_by_truncate(&self, offset: u64, length: u64) -> io::Result<bool> {
-        let mut file = self.file.write().unwrap();
-        let Ok(size) = file.seek(SeekFrom::End(0)) else {
-            // Failed to get length?  Try normal discard.
-            return Ok(false);
-        };
+        // Prevent modifications to the file length
+        #[allow(clippy::readonly_write_lock)]
+        let file = self.file.write().unwrap();
 
+        let size = self.size.load(Ordering::Relaxed);
         if offset >= size {
             // Nothing to do
             return Ok(true);

src/io_buffers.rs
@@ -482,7 +482,10 @@ pub trait IoVectorTrait: Sized {
     ///
     /// Each buffer must be aligned to `mem_alignment`, and each buffer’s length must be aligned to
     /// both `mem_alignment` and `req_alignment` (the I/O request offset/size alignment).
-    fn is_aligned(&self, mem_alignment: usize, req_alignment: usize) -> bool;
+    ///
+    /// If `ignore_end` is set, ignore the length alignment of the last buffer (for the end of the
+    /// file).
+    fn is_aligned(&self, mem_alignment: usize, req_alignment: usize, ignore_end: bool) -> bool;
 
     /// Return the internal vector of `IoSlice` objects.
     fn into_inner(self) -> Vec<Self::BufferType>;
@@ -588,7 +591,7 @@ macro_rules! impl_io_vector {
                 }
             }
 
-            fn is_aligned(&self, mem_alignment: usize, req_alignment: usize) -> bool {
+            fn is_aligned(&self, mem_alignment: usize, req_alignment: usize, ignore_end: bool) -> bool {
                 // Trivial case
                 if mem_alignment == 1 && req_alignment == 1 {
                     return true;
@@ -598,10 +601,18 @@ macro_rules! impl_io_vector {
                 let base_align_mask = mem_alignment - 1;
                 let len_align_mask = base_align_mask | (req_alignment - 1);
 
-                self.vector.iter().all(|buf| {
-                    buf.as_ptr() as usize & base_align_mask == 0 &&
-                        buf.len() & len_align_mask == 0
-                })
+                if ignore_end {
+                    let buffer_count = self.vector.len();
+                    self.vector.iter().enumerate().all(|(i, buf)| {
+                        buf.as_ptr() as usize & base_align_mask == 0 &&
+                            (i == buffer_count - 1 || buf.len() & len_align_mask == 0)
+                    })
+                } else {
+                    self.vector.iter().all(|buf| {
+                        buf.as_ptr() as usize & base_align_mask == 0 &&
+                            buf.len() & len_align_mask == 0
+                    })
+                }
             }
 
             fn into_inner(self) -> Vec<Self::BufferType> {

src/storage.rs
@@ -120,6 +120,36 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     async fn sync(&self) -> io::Result<()>;
 }
 
+/// Check whether the given request is aligned.
+fn is_aligned<V: IoVectorTrait>(
+    bufv: &V,
+    offset: u64,
+    mem_align: usize,
+    req_align: usize,
+    size: Option<u64>,
+) -> io::Result<bool> {
+    debug_assert!(mem_align.is_power_of_two() && req_align.is_power_of_two());
+
+    let req_align_mask = req_align as u64 - 1;
+
+    Ok(if offset & req_align_mask != 0 {
+        false
+    } else if bufv.len() & req_align_mask == 0 {
+        bufv.is_aligned(mem_align, req_align, false)
+    } else if bufv.is_aligned(mem_align, req_align, true) {
+        if let Some(size) = size {
+            let end = offset
+                .checked_add(bufv.len())
+                .ok_or_else(|| io::Error::other("Write wrap-around"))?;
+            end == size
+        } else {
+            false
+        }
+    } else {
+        false
+    })
+}
+
 /// Helper methods for storage objects.
 ///
 /// Provides some simpler methods for accessing storage objects.
@@ -141,21 +171,24 @@ pub trait StorageExt: Storage {
 
         let mem_align = self.mem_align();
         let req_align = self.req_align();
-        let req_align_mask = req_align as u64 - 1;
 
-        debug_assert!(mem_align.is_power_of_two() && req_align.is_power_of_two());
-
-        if offset & req_align_mask == 0
-            && bufv.len() & req_align_mask == 0
-            && bufv.is_aligned(mem_align, req_align)
-        {
+        if is_aligned(&bufv, offset, mem_align, req_align, self.size().ok())? {
             return self.readv(bufv, offset).await;
         }
 
-        trace!("Unaligned read: 0x{:x} + {}", offset, bufv.len());
+        let req_align_mask = req_align as u64 - 1;
+
+        trace!(
+            "Unaligned read: 0x{:x} + {} (size: {:#x})",
+            offset,
+            bufv.len(),
+            self.size().unwrap()
+        );
 
         let unpadded_end = offset + bufv.len();
         let padded_offset = offset & !req_align_mask;
+        // This will over-align at the end of file (aligning to exactly the end of file would be
+        // sufficient), but it is easier this way.
         let padded_end = (unpadded_end + req_align_mask) & !req_align_mask;
 
         trace!(
@@ -198,14 +231,8 @@ pub trait StorageExt: Storage {
 
         let mem_align = self.mem_align();
         let req_align = self.req_align();
-        let req_align_mask = req_align as u64 - 1;
 
-        debug_assert!(mem_align.is_power_of_two() && req_align.is_power_of_two());
-
-        if offset & req_align_mask == 0
-            && bufv.len() & req_align_mask == 0
-            && bufv.is_aligned(mem_align, req_align)
-        {
+        if is_aligned(&bufv, offset, mem_align, req_align, self.size().ok())? {
             return self.writev(bufv, offset).await;
         }
 

commit be6cd775b628b536867092c8d3d3ece969c0a6fc

file: Fix post-EOF read access
Fixes: 196b7e09619758ab305f122bc3915c6757084316
       (“Fix alignment at the end of file and post-EOF read”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/file.rs
@@ -131,12 +131,12 @@ impl Storage for File {
 
     #[cfg(unix)]
     async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
-        let sz = self.size.load(Ordering::Relaxed);
+        let sz: u64 = self.size.load(Ordering::Relaxed);
         let end = offset
             .checked_add(bufv.len())
             .ok_or_else(|| io::Error::other("Read offset overflow"))?;
         let bufv = if end > sz {
-            let (head, mut tail) = bufv.split_at(sz - offset);
+            let (head, mut tail) = bufv.split_at(sz.saturating_sub(offset));
             tail.fill(0);
             head
         } else {