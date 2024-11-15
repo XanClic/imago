XanClic/imago

commit 3b820f3a1ab8e0f7ce865836584fa8594b744822

Hide IoBufferRefTrait
Just like IoVectorTrait, we don’t want users to have to import this
trait for basic functionality, so have it just duplicate what is
primarily implemented directly on the IoBufferRef and IoBufferMut
objects.

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/io_buffers.rs
@@ -126,7 +126,7 @@ impl IoBuffer {
 
     /// Generate an immutable reference to a sub-range.
     pub fn as_ref_range(&self, range: Range<usize>) -> IoBufferRef<'_> {
-        IoBufferRef::from_slice::<u8>(&self.as_ref().into_slice()[range])
+        IoBufferRef::from_slice(&self.as_ref().into_slice()[range])
     }
 
     /// Generate a mutable reference.
@@ -165,7 +165,7 @@ pub trait IoBufferRefTrait<'a>: Sized {
     type PointerType<T: Copy + Sized + 'a>;
 
     /// Create a reference to a slice.
-    fn from_slice<T: Sized>(slice: Self::SliceType<u8>) -> Self;
+    fn from_slice(slice: Self::SliceType<u8>) -> Self;
 
     /// Create an owned [`IoBuffer`] with the same data (copied).
     fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer>;
@@ -209,11 +209,9 @@ pub trait IoBufferRefTrait<'a>: Sized {
     fn into_ref(self) -> IoBufferRef<'a>;
 }
 
-impl<'a> IoBufferRefTrait<'a> for IoBufferRef<'a> {
-    type SliceType<T: Copy + Sized + 'a> = &'a [T];
-    type PointerType<T: Copy + Sized + 'a> = *const T;
-
-    fn from_slice<T: Sized>(slice: &'a [u8]) -> Self {
+impl<'a> IoBufferRef<'a> {
+    /// Create a reference to a slice.
+    pub fn from_slice(slice: &'a [u8]) -> Self {
         IoBufferRef {
             pointer: slice.as_ptr(),
             size: size_of_val(slice),
@@ -221,7 +219,8 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferRef<'a> {
         }
     }
 
-    fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer> {
+    /// Create an owned [`IoBuffer`] with the same data (copied).
+    pub fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer> {
         let mut new_buf = IoBuffer::new(self.len(), alignment)?;
         new_buf
             .as_mut()
@@ -230,26 +229,48 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferRef<'a> {
         Ok(new_buf)
     }
 
-    fn len(&self) -> usize {
+    /// Size in bytes.
+    pub fn len(&self) -> usize {
         self.size
     }
 
-    fn as_ptr(&self) -> *const u8 {
+    /// Whether the length is 0.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Return the pointer to the start of the buffer.
+    pub fn as_ptr(&self) -> *const u8 {
         self.pointer
     }
 
-    unsafe fn into_typed_slice<T: Copy + Sized>(self) -> Self::SliceType<T> {
-        // Safety ensured by the caller; we ensure that nothing outside of this buffer will be part
-        // of the slice
-        unsafe {
-            slice::from_raw_parts(
-                self.as_ptr() as Self::PointerType<T>,
-                self.len() / size_of::<T>(),
-            )
-        }
+    /// Turn this reference into a slice.
+    ///
+    /// References to `IoBuffer`s must not be copied/cloned (so they can only be accessed once;
+    /// they are considered volatile due to potential VM guest accesses), so this consumes the
+    /// object.
+    pub fn into_slice(self) -> &'a [u8] {
+        // Alignment requirement is always met, resulting data is pure binary data
+        unsafe { self.into_typed_slice::<u8>() }
     }
 
-    fn split_at(self, mid: usize) -> (IoBufferRef<'a>, IoBufferRef<'a>) {
+    /// Turn this reference into a slice with the given element type.
+    ///
+    /// # Safety
+    /// Caller must ensure that alignment and length requirements are met and that the resulting
+    /// data is valid.
+    pub unsafe fn into_typed_slice<T: Copy + Sized>(self) -> &'a [T] {
+        // Safety ensured by the caller; we ensure that nothing outside of this buffer will be part
+        // of the slice
+        unsafe { slice::from_raw_parts(self.as_ptr() as *const T, self.len() / size_of::<T>()) }
+    }
+
+    /// Split the buffer at `mid`.
+    ///
+    /// Return `&self[..mid]` and `&self[mid..]`.
+    ///
+    /// If `mid > self.len()`, return `&self[..]` and `[]`.
+    pub fn split_at(self, mid: usize) -> (IoBufferRef<'a>, IoBufferRef<'a>) {
         let head_len = cmp::min(mid, self.size);
 
         (
@@ -267,8 +288,25 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferRef<'a> {
         )
     }
 
-    fn into_ref(self) -> IoBufferRef<'a> {
+    /// Make this reference immutable.
+    pub fn into_ref(self) -> IoBufferRef<'a> {
         self
+    }
+}
+
+impl<'a> IoBufferRefTrait<'a> for IoBufferRef<'a> {
+    type SliceType<T: Copy + Sized + 'a> = &'a [T];
+    type PointerType<T: Copy + Sized + 'a> = *const T;
+
+    passthrough_trait_fn! { fn from_slice(slice: Self::SliceType<u8>) -> Self; }
+    passthrough_trait_fn! { fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer>; }
+    passthrough_trait_fn! { fn len(&self) -> usize; }
+    passthrough_trait_fn! { fn as_ptr(&self) -> Self::PointerType<u8>; }
+    passthrough_trait_fn! { fn split_at(self, mid: usize) -> (Self, Self); }
+    passthrough_trait_fn! { fn into_ref(self) -> IoBufferRef<'a>; }
+
+    unsafe fn into_typed_slice<T: Copy + Sized>(self) -> Self::SliceType<T> {
+        Self::into_typed_slice(self)
     }
 }
 
@@ -288,11 +326,9 @@ impl<'a> From<IoBufferRef<'a>> for IoSlice<'a> {
     }
 }
 
-impl<'a> IoBufferRefTrait<'a> for IoBufferMut<'a> {
-    type SliceType<T: Copy + Sized + 'a> = &'a mut [T];
-    type PointerType<T: Copy + Sized + 'a> = *mut T;
-
-    fn from_slice<T: Sized>(slice: &'a mut [u8]) -> Self {
+impl<'a> IoBufferMut<'a> {
+    /// Create a reference to a slice.
+    pub fn from_slice(slice: &'a mut [u8]) -> Self {
         IoBufferMut {
             pointer: slice.as_mut_ptr(),
             size: size_of_val(slice),
@@ -300,7 +336,8 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferMut<'a> {
         }
     }
 
-    fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer> {
+    /// Create an owned [`IoBuffer`] with the same data (copied).
+    pub fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer> {
         let mut new_buf = IoBuffer::new(self.len(), alignment)?;
         new_buf
             .as_mut()
@@ -309,34 +346,48 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferMut<'a> {
         Ok(new_buf)
     }
 
-    fn len(&self) -> usize {
+    /// Size in bytes.
+    pub fn len(&self) -> usize {
         self.size
     }
 
-    fn as_ptr(&self) -> *mut u8 {
+    /// Whether the length is 0.
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Return the pointer to the start of the buffer.
+    pub fn as_ptr(&self) -> *mut u8 {
         self.pointer
     }
 
-    unsafe fn into_typed_slice<T: Copy + Sized>(self) -> Self::SliceType<T> {
+    /// Turn this reference into a slice.
+    ///
+    /// References to `IoBuffer`s must not be copied/cloned (so they can only be accessed once;
+    /// they are considered volatile due to potential VM guest accesses), so this consumes the
+    /// object.
+    pub fn into_slice(self) -> &'a mut [u8] {
+        // Alignment requirement is always met, resulting data is pure binary data
+        unsafe { self.into_typed_slice::<u8>() }
+    }
+
+    /// Turn this reference into a slice with the given element type.
+    ///
+    /// # Safety
+    /// Caller must ensure that alignment and length requirements are met and that the resulting
+    /// data is valid.
+    pub unsafe fn into_typed_slice<T: Copy + Sized>(self) -> &'a mut [T] {
         // Safety ensured by the caller; we ensure that nothing outside of this buffer will be part
         // of the slice
-        unsafe {
-            slice::from_raw_parts_mut(
-                self.as_ptr() as Self::PointerType<T>,
-                self.len() / size_of::<T>(),
-            )
-        }
+        unsafe { slice::from_raw_parts_mut(self.as_ptr() as *mut T, self.len() / size_of::<T>()) }
     }
 
-    fn into_ref(self) -> IoBufferRef<'a> {
-        IoBufferRef {
-            pointer: self.pointer,
-            size: self.size,
-            _lifetime: PhantomData,
-        }
-    }
-
-    fn split_at(self, mid: usize) -> (IoBufferMut<'a>, IoBufferMut<'a>) {
+    /// Split the buffer at `mid`.
+    ///
+    /// Return `&self[..mid]` and `&self[mid..]`.
+    ///
+    /// If `mid > self.len()`, return `&self[..]` and `[]`.
+    pub fn split_at(self, mid: usize) -> (IoBufferMut<'a>, IoBufferMut<'a>) {
         let head_len = cmp::min(mid, self.size);
 
         (
@@ -352,6 +403,31 @@ impl<'a> IoBufferRefTrait<'a> for IoBufferMut<'a> {
                 _lifetime: PhantomData,
             },
         )
+    }
+
+    /// Make this reference immutable.
+    pub fn into_ref(self) -> IoBufferRef<'a> {
+        IoBufferRef {
+            pointer: self.pointer,
+            size: self.size,
+            _lifetime: PhantomData,
+        }
+    }
+}
+
+impl<'a> IoBufferRefTrait<'a> for IoBufferMut<'a> {
+    type SliceType<T: Copy + Sized + 'a> = &'a mut [T];
+    type PointerType<T: Copy + Sized + 'a> = *mut T;
+
+    passthrough_trait_fn! { fn from_slice(slice: Self::SliceType<u8>) -> Self; }
+    passthrough_trait_fn! { fn try_into_owned(self, alignment: usize) -> io::Result<IoBuffer>; }
+    passthrough_trait_fn! { fn len(&self) -> usize; }
+    passthrough_trait_fn! { fn as_ptr(&self) -> Self::PointerType<u8>; }
+    passthrough_trait_fn! { fn split_at(self, mid: usize) -> (Self, Self); }
+    passthrough_trait_fn! { fn into_ref(self) -> IoBufferRef<'a>; }
+
+    unsafe fn into_typed_slice<T: Copy + Sized>(self) -> Self::SliceType<T> {
+        Self::into_typed_slice(self)
     }
 }
 

src/qcow2/compressed.rs
@@ -1,7 +1,7 @@
 //! Support for compressed clusters.
 
 use super::*;
-use crate::io_buffers::{IoBuffer, IoBufferRefTrait};
+use crate::io_buffers::IoBuffer;
 use miniz_oxide::inflate::core::{decompress as inflate, DecompressorOxide};
 use miniz_oxide::inflate::TINFLStatus;
 

src/qcow2/cow.rs
@@ -3,7 +3,7 @@
 //! Implements copy-on-write when writing to clusters that are not simple allocated data clusters.
 
 use super::*;
-use crate::io_buffers::{IoBuffer, IoBufferRefTrait};
+use crate::io_buffers::IoBuffer;
 
 impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
     /// Do copy-on-write for the given guest cluster, if necessary.

src/qcow2/io_func.rs
@@ -5,7 +5,7 @@
 //! by image drivers (like handling compression).
 
 use super::*;
-use crate::io_buffers::{IoBuffer, IoBufferRefTrait};
+use crate::io_buffers::IoBuffer;
 
 impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
     /// Read the special range at `offset`.

src/qcow2/metadata.rs
@@ -1,7 +1,7 @@
 //! Functionality for working with qcow2 metadata.
 
 use super::types::*;
-use crate::io_buffers::{IoBuffer, IoBufferRefTrait};
+use crate::io_buffers::IoBuffer;
 use crate::macros::numerical_enum;
 use crate::{Storage, StorageExt};
 use bincode::Options;

src/storage/ext.rs
@@ -5,9 +5,7 @@
 //! unaligned requests and provides write serialization.
 
 use super::drivers::RangeBlockedGuard;
-use crate::io_buffers::{
-    IoBuffer, IoBufferRefTrait, IoVector, IoVectorBounceBuffers, IoVectorMut, IoVectorTrait,
-};
+use crate::io_buffers::{IoBuffer, IoVector, IoVectorBounceBuffers, IoVectorMut, IoVectorTrait};
 use crate::Storage;
 use std::ops::Range;
 use std::{cmp, io};

src/storage/mod.rs
@@ -6,7 +6,7 @@
 pub(crate) mod drivers;
 pub mod ext;
 
-use crate::io_buffers::{IoBuffer, IoBufferRefTrait, IoVector, IoVectorMut};
+use crate::io_buffers::{IoBuffer, IoVector, IoVectorMut};
 use drivers::CommonStorageHelper;
 use ext::StorageExt;
 use std::fmt::{Debug, Display};

commit 13457ca45a92125636d6bfabe9e3e104f4fee3ba

Really hide IoBufferRefTrait
Forgot to actually make the trait local to this crate.

This is a breaking change, unfortunately, but better earlier than later.
Add a prerelease identifier to the version string to mark that.

Fixes: 3b820f3a1ab8e0f7ce865836584fa8594b744822
       (“Hide IoBufferRefTrait”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "imago"
-version = "0.1.0"
+version = "0.1.0-modified"
 edition = "2021"
 license = "MIT"
 description = "A library for accessing virtual machine disk images."

src/io_buffers.rs
@@ -159,7 +159,8 @@ impl Drop for IoBuffer {
 }
 
 /// Common functions for both `IoBufferRef` and `IoBufferMut`.
-pub trait IoBufferRefTrait<'a>: Sized {
+#[allow(dead_code)]
+pub(crate) trait IoBufferRefTrait<'a>: Sized {
     /// `&[T]` or `&mut [T]`.
     type SliceType<T: Copy + Sized + 'a>;
 