XanClic/imago

commit ac9ae6c01439c8d979bac9657e72f605c9a2c1b3

Add probing
Currently, there is no over-arching function to probe any format, but
each must be probed separately (which is enough for QEMU integration).

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -142,12 +142,6 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
         Self::open_image(metadata, writable).await
     }
 
-    /// Check whether the given image file is a qcow2 file.
-    pub(crate) async fn probe(metadata: &S) -> io::Result<()> {
-        Header::load(metadata, true).await?;
-        Ok(())
-    }
-
     /// Does this qcow2 image require an external data file?
     ///
     /// Conversely, if this is `false`, this image must not use an external data file.
@@ -320,6 +314,18 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
 #[async_trait(?Send)]
 impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
     type Storage = S;
+
+    async fn probe(metadata: &S) -> io::Result<bool>
+    where
+        Self: Sized,
+    {
+        let mut magic_version = [0u8; 8];
+        metadata.read(&mut magic_version[..], 0).await?;
+
+        let magic = u32::from_be_bytes((&magic_version[..4]).try_into().unwrap());
+        let version = u32::from_be_bytes((&magic_version[4..]).try_into().unwrap());
+        Ok(magic == MAGIC && (version == 2 || (version == 3)))
+    }
 
     fn size(&self) -> u64 {
         self.header.size()

src/format/drivers.rs
@@ -15,6 +15,15 @@ pub trait FormatDriverInstance: Debug + Display + Send + Sync {
     /// Type of storage used.
     type Storage: Storage;
 
+    /// Check whether `storage` has this format.
+    ///
+    /// This is only a rough test and does not guarantee that opening `storage` under this format
+    /// will succeed.  Generally, it will only check the magic bytes (if available).  For formats
+    /// that do not have distinct features (like raw), this will always return `true`.
+    async fn probe(storage: &Self::Storage) -> io::Result<bool>
+    where
+        Self: Sized;
+
     /// Size of the disk represented by this image.
     fn size(&self) -> u64;
 

src/qcow2/metadata.rs
@@ -16,7 +16,7 @@ use tokio::sync::{Mutex, MutexGuard};
 use tracing::error;
 
 /// Qcow header magic ("QFI\xfb").
-const MAGIC: u32 = 0x51_46_49_fb;
+pub(super) const MAGIC: u32 = 0x51_46_49_fb;
 
 /// Maximum permissible host offset.
 pub(super) const MAX_OFFSET: HostOffset = HostOffset(0x00ff_ffff_ffff_fe00u64);

src/raw.rs
@@ -63,6 +63,13 @@ impl<S: Storage> Raw<S> {
 impl<S: Storage> FormatDriverInstance for Raw<S> {
     type Storage = S;
 
+    async fn probe(_storage: &S) -> io::Result<bool>
+    where
+        Self: Sized,
+    {
+        Ok(true)
+    }
+
     fn size(&self) -> u64 {
         self.size
     }

commit 458226def97aff081eef08ae6cc841ea94359a61

qcow2: Fix probing
The probing function changed, fix the `probe()` call accordingly.

Fixes: ac9ae6c01439c8d979bac9657e72f605c9a2c1b3
       (“Add probing”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -260,13 +260,11 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
 
             Some(fmt) => Err(io::Error::other(format!("Unknown backing format {fmt}"))),
 
-            None => {
-                if Self::probe(&file).await.is_ok() {
-                    self.open_qcow2_backing_file(file).await.map(Some)
-                } else {
-                    self.open_raw_backing_file(file).await.map(Some)
-                }
-            }
+            None => match Self::probe(&file).await {
+                Ok(true) => self.open_qcow2_backing_file(file).await.map(Some),
+                Ok(false) => self.open_raw_backing_file(file).await.map(Some),
+                Err(err) => Err(err),
+            },
         };
 
         result.err_context(|| format!("Backing file {absolute:?}"))