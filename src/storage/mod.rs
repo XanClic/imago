XanClic/imago

commit 77562ddeb7aff0f844d88546568837673e7adc00

Storage: Create write_full_zeroes() helper
Preallocation functions implementing write-zeroes-as-data as a
preallocation method will want this.

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/storage/mod.rs
@@ -6,15 +6,14 @@
 pub mod drivers;
 pub mod ext;
 
-use crate::io_buffers::{IoBuffer, IoVector, IoVectorMut};
+use crate::io_buffers::{IoVector, IoVectorMut};
 use drivers::CommonStorageHelper;
-use ext::StorageExt;
 use std::fmt::{Debug, Display};
 use std::future::Future;
+use std::io;
 use std::path::{Path, PathBuf};
 use std::pin::Pin;
 use std::sync::Arc;
-use std::{cmp, io};
 
 /// Parameters from which a storage object can be constructed.
 #[derive(Clone, Default)]
@@ -140,20 +139,8 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     ///
     /// Use [`StorageExt::write_zeroes()`] instead.
     #[allow(async_fn_in_trait)] // No need for Send
-    async unsafe fn pure_write_zeroes(&self, mut offset: u64, mut length: u64) -> io::Result<()> {
-        let buflen = cmp::min(length, 1048576) as usize;
-        let mut buf = IoBuffer::new(buflen, self.mem_align())?;
-        buf.as_mut().into_slice().fill(0);
-
-        while length > 0 {
-            let chunk_length = cmp::min(length, 1048576) as usize;
-            self.writev(buf.as_ref_range(0..chunk_length).into(), offset)
-                .await?;
-            offset += chunk_length as u64;
-            length -= chunk_length as u64;
-        }
-
-        Ok(())
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        ext::write_full_zeroes(self, offset, length).await
     }
 
     /// Discard the given range, with undefined contents when read back.

src/storage/ext.rs
@@ -336,3 +336,34 @@ fn is_aligned<V: IoVectorTrait>(bufv: &V, offset: u64, mem_align: usize, req_ali
         false
     }
 }
+
+/// Write zero data to the given area.
+///
+/// In contrast to `write_zeroes()` functions, this one will actually write zero data, fully
+/// allocated.
+pub(crate) async fn write_full_zeroes<S: StorageExt>(
+    storage: S,
+    mut offset: u64,
+    mut length: u64,
+) -> io::Result<()> {
+    let buflen = cmp::min(length, 1048576) as usize;
+    let mut buf = IoBuffer::new(buflen, storage.mem_align())?;
+    buf.as_mut().into_slice().fill(0);
+
+    let req_align = storage.req_align();
+    let req_align_mask = (req_align - 1) as u64;
+
+    while length > 0 {
+        let mut chunk_length = cmp::min(length, 1048576) as usize;
+        if offset & req_align_mask != 0 {
+            chunk_length = cmp::min(chunk_length, req_align - (offset & req_align_mask) as usize);
+        }
+        storage
+            .write(buf.as_ref_range(0..chunk_length), offset)
+            .await?;
+        offset += chunk_length as u64;
+        length -= chunk_length as u64;
+    }
+
+    Ok(())
+}

commit 6fdcaf7aa45c6f57dc1cecd90cb484372f60ac54

Storage: Fix doc links
With the removal of the `StorageExt` dependency, the links pointing
there must be fixed up.

Fixes: 77562ddeb7aff0f844d88546568837673e7adc00
       (“Storage: Create write_full_zeroes() helper”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/storage/mod.rs
@@ -106,7 +106,7 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     /// [`Self::mem_align()`] and [`Self::req_align()`], and safeguards we want to implement for
     /// safe concurrent access may not be available.
     ///
-    /// Use [`StorageExt::readv()`] instead.
+    /// Use [`StorageExt::readv()`](crate::StorageExt::readv()) instead.
     #[allow(async_fn_in_trait)] // No need for Send
     async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()>;
 
@@ -123,7 +123,7 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     /// [`Self::mem_align()`] and [`Self::req_align()`], and safeguards we want to implement for
     /// safe concurrent access may not be available.
     ///
-    /// Use [`StorageExt::writev()`] instead.
+    /// Use [`StorageExt::writev()`](crate::StorageExt::writev()) instead.
     #[allow(async_fn_in_trait)] // No need for Send
     async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()>;
 
@@ -137,7 +137,7 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     /// [`Self::zero_align()`], and safeguards we want to implement for safe concurrent access may
     /// not be available.
     ///
-    /// Use [`StorageExt::write_zeroes()`] instead.
+    /// Use [`StorageExt::write_zeroes()`](crate::StorageExt::write_zeroes()) instead.
     #[allow(async_fn_in_trait)] // No need for Send
     async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
         ext::write_full_zeroes(self, offset, length).await
@@ -155,7 +155,7 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     /// [`Self::discard_align()`], and safeguards we want to implement for safe concurrent access
     /// may not be available.
     ///
-    /// Use [`StorageExt::discard()`] instead.
+    /// Use [`StorageExt::discard()`](crate::StorageExt::discard()) instead.
     #[allow(async_fn_in_trait)] // No need for Send
     async unsafe fn pure_discard(&self, _offset: u64, _length: u64) -> io::Result<()> {
         Ok(())
@@ -189,7 +189,8 @@ pub trait Storage: Debug + Display + Send + Sized + Sync {
     #[allow(async_fn_in_trait)] // No need for Send
     async unsafe fn invalidate_cache(&self) -> io::Result<()>;
 
-    /// Return the storage helper object (used by the [`StorageExt`] implementation).
+    /// Return the storage helper object (used by the [`StorageExt`](crate::StorageExt)
+    /// implementation).
     fn get_storage_helper(&self) -> &CommonStorageHelper;
 
     /// Resize to the given size.