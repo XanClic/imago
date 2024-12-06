XanClic/imago

commit 439de28e8db3fc54e016df4b6db5d80d1d6ed1d2

Formats: Add resizing
Add interfaces for resizing.  Shrinking requires, just like discard, a
mutable `self` reference, ensuring that existing data mappings may be
invalidated (because the old tail of the image will be discarded).

Implement resizing for qcow2 and raw.

Fixes: #7
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -8,6 +8,7 @@ mod cow;
 mod io_func;
 mod mappings;
 mod metadata;
+mod preallocation;
 #[cfg(feature = "sync-wrappers")]
 mod sync_wrappers;
 mod types;
@@ -17,11 +18,11 @@ use crate::format::builder::FormatDriverBuilder;
 use crate::format::drivers::{FormatDriverInstance, Mapping};
 use crate::format::gate::{ImplicitOpenGate, PermissiveImplicitOpenGate};
 use crate::format::wrapped::WrappedFormat;
-use crate::format::Format;
+use crate::format::{Format, PreallocateMode};
 use crate::io_buffers::IoVectorMut;
 use crate::misc_helpers::ResultErrorContext;
 use crate::raw::Raw;
-use crate::{FormatAccess, Storage, StorageExt, StorageOpenOptions};
+use crate::{storage, FormatAccess, Storage, StorageExt, StorageOpenOptions};
 use allocation::Allocator;
 use async_trait::async_trait;
 pub use builder::Qcow2OpenBuilder;
@@ -603,6 +604,76 @@ impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
         .await?;
 
         Ok(())
+    }
+
+    async fn resize_grow(&self, new_size: u64, prealloc_mode: PreallocateMode) -> io::Result<()> {
+        self.need_writable()?;
+
+        let old_size = self.size();
+        let grown_length = new_size.saturating_sub(old_size);
+        if grown_length == 0 {
+            return Ok(()); // only grow, else do nothing
+        }
+
+        // Grow before preallocating (so we can preallocate)
+        self.header.set_size(new_size);
+
+        match prealloc_mode {
+            PreallocateMode::None => Ok(()),
+            PreallocateMode::Zero => self.preallocate_zero(old_size, grown_length).await,
+            PreallocateMode::FormatAllocate => {
+                self.preallocate(old_size, grown_length, storage::PreallocateMode::Zero)
+                    .await
+            }
+            PreallocateMode::FullAllocate => {
+                self.preallocate(old_size, grown_length, storage::PreallocateMode::Allocate)
+                    .await
+            }
+            PreallocateMode::WriteData => self.preallocate_write_data(old_size, grown_length).await,
+        }
+        .inspect_err(|_| {
+            // Better reset to old size then
+            self.header.set_size(old_size)
+        })?;
+
+        // Do this last because we may not be able to undo it
+        self.header
+            .write_size(self.metadata.as_ref())
+            .await
+            .inspect_err(|_| {
+                // Reset to old size
+                self.header.set_size(old_size)
+            })
+    }
+
+    async fn resize_shrink(&mut self, new_size: u64) -> io::Result<()> {
+        self.need_writable()?;
+
+        let old_size = self.size();
+        if new_size >= old_size {
+            return Ok(()); // only shrink, else do nothing
+        }
+
+        let mut offset = new_size;
+        while offset < old_size {
+            match self.discard_to_backing(offset, old_size - offset).await {
+                Ok((dofs, dlen)) => offset = dofs + dlen,
+                // Basically ignore errors, but stop trying to discard
+                Err(_) => break,
+            }
+        }
+
+        // Shrink after discarding (so we can discard)
+        self.header.set_size(new_size);
+
+        // Do this last because we may not be able to undo it
+        self.header
+            .write_size(self.metadata.as_ref())
+            .await
+            .inspect_err(|_| {
+                // Reset to old size
+                self.header.set_size(old_size);
+            })
     }
 }
 

src/format/mod.rs
@@ -20,3 +20,37 @@ pub enum Format {
     /// Qcow2 format (version 2 or 3)
     Qcow2,
 }
+
+/// Format layer preallocation modes.
+///
+/// When resizing or create an image, this mode determines whether and how the new data range is to
+/// be preallocated.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum PreallocateMode {
+    /// No preallocation.
+    ///
+    /// Reading the new range may return random data.
+    None,
+
+    /// Ensure range reads as zeroes.
+    ///
+    /// Does not necessarily allocate data, but has to ensure the new range will read back as
+    /// zeroes (e.g. a backing file’s contents must not show through).
+    Zero,
+
+    /// Metadata preallocation.
+    ///
+    /// Do not write data, but ensure all blocks are mapped as data.  They must read as zero still.
+    FormatAllocate,
+
+    /// Metadata and extent preallocation.
+    ///
+    /// Same as `FormatAllocate`, but also allocate all blocks on the underlying storage.
+    FullAllocate,
+
+    /// Full data preallocation.
+    ///
+    /// Write zeroes to the whole range.
+    WriteData,
+}

src/format/access.rs
@@ -3,6 +3,7 @@
 //! Provides access to different image formats via `FormatAccess` objects.
 
 use super::drivers::{self, FormatDriverInstance};
+use super::PreallocateMode;
 use crate::io_buffers::{IoVector, IoVectorMut};
 use crate::storage::ext::write_full_zeroes;
 use crate::vector_select::FutureVector;
@@ -554,7 +555,6 @@ impl<S: Storage> FormatAccess<S> {
     ///
     /// Note that this will not drop the buffers, so they may still be used to serve later
     /// accesses.  Use [`FormatAccess::invalidate_cache()`] to drop all buffers.
-    #[allow(async_fn_in_trait)] // No need for Send
     pub async fn flush(&self) -> io::Result<()> {
         self.inner.flush().await
     }
@@ -563,7 +563,6 @@ impl<S: Storage> FormatAccess<S> {
     ///
     /// This does not necessarily include flushing internal buffers, i.e. `flush`.  When using
     /// `sync()`, consider whether you want to call `flush()` before it.
-    #[allow(async_fn_in_trait)] // No need for Send
     pub async fn sync(&self) -> io::Result<()> {
         self.inner.sync().await
     }
@@ -576,9 +575,63 @@ impl<S: Storage> FormatAccess<S> {
     /// # Safety
     /// Not flushing internal buffers may cause image corruption.  You must ensure the on-disk
     /// state is consistent.
-    #[allow(async_fn_in_trait)] // No need for Send
     pub async unsafe fn invalidate_cache(&self) -> io::Result<()> {
         self.inner.invalidate_cache().await
+    }
+
+    /// Resize to the given size.
+    ///
+    /// Set the disk size to `new_size`.  If `new_size` is smaller than the current size, ignore
+    /// both preallocation modes and discard the data after `new_size`.
+    ///
+    /// If `new_size` is larger than the current size, `prealloc_mode` determines whether and how
+    /// the new range should be allocated; depending on the image format, is possible some
+    /// preallocation modes are not supported, in which case an [`std::io::ErrorKind::Unsupported`]
+    /// is returned.
+    ///
+    /// This may break existing data mappings, so needs a mutable reference to `self`, which
+    /// ensures that existing data references (which have the lifetime of an immutable `self`
+    /// reference) cannot be kept.
+    ///
+    /// See also [`FormatAccess::resize_grow()`] and [`FormatAccess::resize_shrink()`], whose more
+    /// specialized interface may be useful when you know whether you want to grow or shrink the
+    /// image.
+    pub async fn resize(
+        &mut self,
+        new_size: u64,
+        prealloc_mode: PreallocateMode,
+    ) -> io::Result<()> {
+        match new_size.cmp(&self.size()) {
+            std::cmp::Ordering::Less => self.resize_shrink(new_size).await,
+            std::cmp::Ordering::Equal => Ok(()),
+            std::cmp::Ordering::Greater => self.resize_grow(new_size, prealloc_mode).await,
+        }
+    }
+
+    /// Resize to the given size, which must be greater than the current size.
+    ///
+    /// Set the disk size to `new_size`, preallocating the new space according to `prealloc_mode`.
+    /// Depending on the image format, it is possible some preallocation modes are not supported,
+    /// in which case an [`std::io::ErrorKind::Unsupported`] is returned.
+    ///
+    /// If the current size is already `new_size` or greater, do nothing.
+    pub async fn resize_grow(
+        &self,
+        new_size: u64,
+        prealloc_mode: PreallocateMode,
+    ) -> io::Result<()> {
+        self.inner.resize_grow(new_size, prealloc_mode).await
+    }
+
+    /// Truncate to the given size, which must be smaller than the current size.
+    ///
+    /// Set the disk size to `new_size`, discarding the data after `new_size`.
+    ///
+    /// May break existing data mappings thanks to the mutable `self` reference.
+    ///
+    /// If the current size is already `new_size` or smaller, do nothing.
+    pub async fn resize_shrink(&mut self, new_size: u64) -> io::Result<()> {
+        self.inner.resize_shrink(new_size).await
     }
 }
 

src/format/drivers.rs
@@ -3,7 +3,7 @@
 //! Provides the internal interface for image format drivers to provide their services, on which
 //! the publically visible interface [`FormatAccess`] is built.
 
-use super::Format;
+use super::{Format, PreallocateMode};
 use crate::io_buffers::IoVectorMut;
 use crate::{FormatAccess, Storage};
 use async_trait::async_trait;
@@ -186,6 +186,24 @@ pub trait FormatDriverInstance: Debug + Display + Send + Sync {
     /// Not flushing internal buffers may cause image corruption.  The caller must ensure the
     /// on-disk state is consistent.
     async unsafe fn invalidate_cache(&self) -> io::Result<()>;
+
+    /// Resize to the given size, which must be greater than the current size.
+    ///
+    /// Set the disk size to `new_size`, preallocating the new space according to `prealloc_mode`.
+    /// Depending on the image format, it is possible some preallocation modes are not supported,
+    /// in which case an [`std::io::ErrorKind::Unsupported`] is returned.
+    ///
+    /// If the current size is already `new_size` or greater, do nothing.
+    async fn resize_grow(&self, new_size: u64, prealloc_mode: PreallocateMode) -> io::Result<()>;
+
+    /// Truncate to the given size, which must be smaller than the current size.
+    ///
+    /// Set the disk size to `new_size`, discarding the data after `new_size`.
+    ///
+    /// May break existing data mappings thanks to the mutable `self` reference.
+    ///
+    /// If the current size is already `new_size` or smaller, do nothing.
+    async fn resize_shrink(&mut self, new_size: u64) -> io::Result<()>;
 }
 
 /// Non-recursive mapping information.

src/format/sync_wrappers.rs
@@ -1,6 +1,7 @@
 //! Synchronous wrapper around [`FormatAccess`].
 
 use super::drivers::FormatDriverInstance;
+use super::PreallocateMode;
 use crate::io_buffers::{IoVector, IoVectorMut};
 use crate::{FormatAccess, Mapping, Storage};
 use std::io;
@@ -144,6 +145,12 @@ impl<S: Storage> SyncFormatAccess<S> {
     /// May use efficient zeroing for a subset of the given range, if supported by the format.
     /// Will not discard anything, which keeps existing data mappings usable, albeit writing to
     /// mappings that are now zeroed may have no effect.
+    ///
+    /// Check if [`SyncFormatAccess::discard_to_zero()`] better suits your needs: It may work
+    /// better on a wider range of formats (`write_zeroes()` requires support for preallocated zero
+    /// clusters, which qcow2 does have, but other formats may not), and can actually free up
+    /// space.  However, because it can break existing data mappings, it requires a mutable `self`
+    /// reference.
     pub fn write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
         self.runtime
             .block_on(self.inner.write_zeroes(offset, length))
@@ -151,7 +158,7 @@ impl<S: Storage> SyncFormatAccess<S> {
 
     /// Discard the given range, ensure it is read back as zeroes.
     ///
-    /// Effectively the same as [`FormatAccess::write_zeroes()`], but discard as much of the
+    /// Effectively the same as [`SyncFormatAccess::write_zeroes()`], but discard as much of the
     /// existing allocation as possible.  This breaks existing data mappings, so needs a mutable
     /// reference to `self`, which ensures that existing data references (which have the lifetime
     /// of an immutable `self` reference) cannot be kept.
@@ -167,7 +174,7 @@ impl<S: Storage> SyncFormatAccess<S> {
     ///
     /// Discard as much of the given range as possible, and keep the rest as-is.  Does not
     /// guarantee any specific data on read-back, in contrast to
-    /// [`FormatAccess::discard_to_zero()`].
+    /// [`SyncFormatAccess::discard_to_zero()`].
     ///
     /// Discarding being unsupported by this format is still returned as an error
     /// ([`std::io::ErrorKind::Unsupported`])
@@ -216,6 +223,47 @@ impl<S: Storage> SyncFormatAccess<S> {
     /// state is consistent.
     pub unsafe fn invalidate_cache(&self) -> io::Result<()> {
         self.runtime.block_on(self.inner.invalidate_cache())
+    }
+
+    /// Resize to the given size.
+    ///
+    /// Set the disk size to `new_size`.  If `new_size` is smaller than the current size, ignore
+    /// both preallocation modes and discard the data after `new_size`.
+    ///
+    /// If `new_size` is larger than the current size, `prealloc_mode` determines whether and how
+    /// the new range should be allocated; depending on the image format, is possible some
+    /// preallocation modes are not supported, in which case an [`std::io::ErrorKind::Unsupported`]
+    /// is returned.
+    ///
+    /// This may break existing data mappings, so needs a mutable reference to `self`, which
+    /// ensures that existing data references (which have the lifetime of an immutable `self`
+    /// reference) cannot be kept.
+    ///
+    /// See also [`SyncFormatAccess::resize_grow()`] and [`SyncFormatAccess::resize_shrink()`],
+    /// whose more specialized interface may be useful when you know whether you want to grow or
+    /// shrink the image.
+    pub fn resize(&mut self, new_size: u64, prealloc_mode: PreallocateMode) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.resize(new_size, prealloc_mode))
+    }
+
+    /// Resize to the given size, which must be greater than the current size.
+    ///
+    /// Set the disk size to `new_size`, preallocating the new space according to `prealloc_mode`.
+    /// Depending on the image format, it is possible some preallocation modes are not supported,
+    /// in which case an [`std::io::ErrorKind::Unsupported`] is returned.
+    pub fn resize_grow(&self, new_size: u64, prealloc_mode: PreallocateMode) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.resize_grow(new_size, prealloc_mode))
+    }
+
+    /// Truncate to the given size, which must be smaller than the current size.
+    ///
+    /// Set the disk size to `new_size`, discarding the data after `new_size`.
+    ///
+    /// May break existing data mappings thanks to the mutable `self` reference.
+    pub fn resize_shrink(&mut self, new_size: u64) -> io::Result<()> {
+        self.runtime.block_on(self.inner.resize_shrink(new_size))
     }
 }
 

src/qcow2/metadata.rs
@@ -58,7 +58,7 @@ struct V2Header {
     /// byte cluster size, it is unable to populate a virtual size larger than 128 GB (37 bits).
     /// Meanwhile, L1/L2 table layouts limit an image to no more than 64 PB (56 bits) of populated
     /// clusters, and an image may hit other limits first (such as a file system’s maximum size).
-    size: u64,
+    size: AtomicU64,
 
     /// Encryption method:
     ///
@@ -588,9 +588,9 @@ impl Header {
         check_field!(v2.backing_file_offset)?; // TODO: Should be mutable
         check_field!(v2.backing_file_size)?; // TODO: Should be mutable
         check_field!(v2.cluster_bits)?;
-        check_field!(v2.size)?; // TODO: Should be mutable
-                                // L1 position is mutable
-                                // Reftable position is mutable
+        // Size is mutable
+        // L1 position is mutable
+        // Reftable position is mutable
         check_field!(v2.crypt_method)?;
         check_field!(v2.nb_snapshots)?; // TODO: Should be mutable
         check_field!(v2.snapshots_offset)?; // TODO: Should be mutable
@@ -614,6 +614,11 @@ impl Header {
             .ok_or_else(|| io::Error::other("Header extensions modified"))?;
 
         check_field!(external_data_file)?;
+
+        self.v2.size.store(
+            new_header.v2.size.load(Ordering::Relaxed),
+            Ordering::Relaxed,
+        );
 
         self.v2.l1_table_offset.store(
             new_header.v2.l1_table_offset.load(Ordering::Relaxed),
@@ -640,7 +645,7 @@ impl Header {
 
     /// Guest disk size.
     pub fn size(&self) -> u64 {
-        self.v2.size
+        self.v2.size.load(Ordering::Relaxed)
     }
 
     /// Require a minimum qcow2 version.
@@ -656,6 +661,11 @@ impl Header {
                 format!("qcow2 version {minimum} required, image has version {version}"),
             ))
         }
+    }
+
+    /// Set the guest disk size.
+    pub fn set_size(&self, new_size: u64) {
+        self.v2.size.store(new_size, Ordering::Relaxed)
     }
 
     /// log2 of the cluster size.
@@ -874,6 +884,12 @@ impl Header {
     /// Write the L1 table pointer (offset and size) to disk.
     pub async fn write_l1_table_pointer<S: Storage>(&self, image: &S) -> io::Result<()> {
         // TODO: Just write the L1 table offset and size
+        self.write_v2_header(image).await
+    }
+
+    /// Write the guest disk size to disk.
+    pub async fn write_size<S: Storage>(&self, image: &S) -> io::Result<()> {
+        // TODO: Just write the size
         self.write_v2_header(image).await
     }
 }

src/raw.rs
@@ -5,12 +5,13 @@
 use crate::format::builder::{FormatDriverBuilder, FormatDriverBuilderBase};
 use crate::format::drivers::{FormatDriverInstance, Mapping};
 use crate::format::gate::ImplicitOpenGate;
-use crate::format::Format;
-use crate::{Storage, StorageExt, StorageOpenOptions};
+use crate::format::{Format, PreallocateMode};
+use crate::{storage, Storage, StorageExt, StorageOpenOptions};
 use async_trait::async_trait;
 use std::fmt::{self, Display, Formatter};
 use std::io;
 use std::path::Path;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 /// Wraps a storage object without any translation.
 #[derive(Debug)]
@@ -22,7 +23,7 @@ pub struct Raw<S: Storage> {
     writable: bool,
 
     /// Disk size, which is the file size when this object was created.
-    size: u64,
+    size: AtomicU64,
 }
 
 impl<S: Storage> Raw<S> {
@@ -42,7 +43,7 @@ impl<S: Storage> Raw<S> {
         Ok(Raw {
             inner,
             writable,
-            size,
+            size: size.into(),
         })
     }
 
@@ -60,7 +61,7 @@ impl<S: Storage> Raw<S> {
         Ok(Raw {
             inner,
             writable,
-            size,
+            size: size.into(),
         })
     }
 
@@ -89,7 +90,7 @@ impl<S: Storage> FormatDriverInstance for Raw<S> {
     }
 
     fn size(&self) -> u64 {
-        self.size
+        self.size.load(Ordering::Relaxed)
     }
 
     fn collect_storage_dependencies(&self) -> Vec<&S> {
@@ -105,7 +106,7 @@ impl<S: Storage> FormatDriverInstance for Raw<S> {
         offset: u64,
         max_length: u64,
     ) -> io::Result<(Mapping<'a, S>, u64)> {
-        let remaining = match self.size.checked_sub(offset) {
+        let remaining = match self.size().checked_sub(offset) {
             None | Some(0) => return Ok((Mapping::Eof, 0)),
             Some(remaining) => remaining,
         };
@@ -126,7 +127,7 @@ impl<S: Storage> FormatDriverInstance for Raw<S> {
         length: u64,
         _overwrite: bool,
     ) -> io::Result<(&'a S, u64, u64)> {
-        let Some(remaining) = self.size.checked_sub(offset) else {
+        let Some(remaining) = self.size().checked_sub(offset) else {
             return Err(io::Error::other("Cannot allocate past the end of file"));
         };
         if length > remaining {
@@ -194,6 +195,48 @@ impl<S: Storage> FormatDriverInstance for Raw<S> {
         // No internal buffers to drop
         // Safe: Caller says we should do this
         unsafe { self.inner.invalidate_cache() }.await
+    }
+
+    async fn resize_grow(
+        &self,
+        new_size: u64,
+        format_prealloc_mode: PreallocateMode,
+    ) -> io::Result<()> {
+        if self
+            .size
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |old| {
+                (new_size > old).then_some(new_size)
+            })
+            .is_err()
+        {
+            return Ok(()); // only grow, else do nothing
+        }
+
+        let storage_prealloc_mode = match format_prealloc_mode {
+            PreallocateMode::None => storage::PreallocateMode::None,
+            PreallocateMode::Zero | PreallocateMode::FormatAllocate => {
+                storage::PreallocateMode::Zero
+            }
+            PreallocateMode::FullAllocate => storage::PreallocateMode::Allocate,
+            PreallocateMode::WriteData => storage::PreallocateMode::WriteData,
+        };
+        self.inner.resize(new_size, storage_prealloc_mode).await
+    }
+
+    async fn resize_shrink(&mut self, new_size: u64) -> io::Result<()> {
+        if self
+            .size
+            .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |old| {
+                (new_size < old).then_some(new_size)
+            })
+            .is_err()
+        {
+            return Ok(()); // only shrink, else do nothing
+        }
+
+        self.inner
+            .resize(new_size, storage::PreallocateMode::None)
+            .await
     }
 }
 

commit 9de6d30e0fe8020a0984d919ed0d97df3a90d7aa

qcow2: Handle undiscardable tail in resize_shrink
If there is a tail left that cannot be discarded, stop trying, lest we
end in an infinite loop.

Fixes: 439de28e8db3fc54e016df4b6db5d80d1d6ed1d2
       (“Formats: Add resizing”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -648,6 +648,7 @@ impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
         let mut offset = new_size;
         while offset < old_size {
             match self.discard_to_backing(offset, old_size - offset).await {
+                Ok((_, 0)) => break, // cannot discard tail
                 Ok((dofs, dlen)) => offset = dofs + dlen,
                 // Basically ignore errors, but stop trying to discard
                 Err(_) => break,