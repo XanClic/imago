XanClic/imago

commit 0ea71defd1c5d423d41a26ef22e0b63116e60ffa

Formats: Add discarding and write-zeroes
We need to be careful with anything that can discard data mappings, as
that may seriously break existing mapping information, i.e. not just
invalidate it, but make them actually dangerous to use: Data mappings
are allowed to be writable, bypassing the format layer, so if one is
invalidated, it must absolutely no longer be written to, or the results
would be undefined.  (For example, the mapping could be repurposed, and
then the data would show up somewhere else, or worse, corrupt metadata.)

Therefore, anything that can discard mappings must take a mutable
reference to `FormatAccess`/`FormatDriverInstance`.  This ensures that
any mapping returned by `get_mapping()` or `ensure_data_mapping()`
cannot be kept across discard operations, because they contain storage
object references, which are bound to the lifetime of an immutable
`FormatAccess`/`FormatDriverInstance` reference.

As an alternative, there is also the non-discarding
`FormatAccess::write_zeroes()` variant (through
`FormatDriverInstance::ensure_zero_mapping()`).

Fixes: #1
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mappings.rs
@@ -150,6 +150,61 @@ impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
         Ok((self.storage(), host_offset, length))
     }
 
+    /// Make the given range be mapped by a fixed kind of clusters.
+    ///
+    /// Allows zeroing or discarding clusters.  `mapping` says which kind of mapping to create.
+    ///
+    /// Return the first cluster affected, and the number of clusters affected (may be 0).
+    pub(super) async fn ensure_fixed_mapping(
+        &self,
+        offset: GuestOffset,
+        length: u64,
+        mapping: FixedMapping,
+    ) -> io::Result<(GuestCluster, ClusterCount)> {
+        match mapping {
+            FixedMapping::ZeroDiscard | FixedMapping::ZeroRetainAllocation => {
+                self.header.require_version(3)?;
+            }
+            FixedMapping::FullDiscard => (),
+        }
+
+        let cb = self.header.cluster_bits();
+
+        // We can only touch full clusters
+        let cluster_align_mask = self.header.cluster_size() as u64 - 1;
+        let aligned_end = (offset + length).0 & !cluster_align_mask;
+        let aligned_offset = (offset + cluster_align_mask).0 & !cluster_align_mask;
+        let aligned_length = aligned_end.saturating_sub(aligned_offset);
+
+        // We have aligned this, so we can unwrap
+        let first_cluster = GuestOffset(aligned_offset).checked_cluster(cb).unwrap();
+        let cluster_count = ClusterCount::checked_from_byte_size(aligned_length, cb).unwrap();
+
+        if cluster_count.0 == 0 {
+            return Ok((first_cluster, cluster_count));
+        }
+
+        let l2_table = self.ensure_l2(first_cluster.offset(cb)).await?;
+        let l2_table = l2_table.lock_write().await;
+        let mut leaked_allocations = Vec::<(HostCluster, ClusterCount)>::new();
+
+        let res = self
+            .ensure_fixed_mapping_no_cleanup(
+                first_cluster,
+                cluster_count,
+                mapping,
+                l2_table,
+                &mut leaked_allocations,
+            )
+            .await;
+
+        for alloc in leaked_allocations {
+            self.free_data_clusters(alloc.0, alloc.1).await;
+        }
+
+        Ok((first_cluster, res?))
+    }
+
     /// Get the L2 table referenced by the given L1 table index, if any.
     ///
     /// `writable` says whether the L2 table should be modifiable.
@@ -349,4 +404,73 @@ impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
 
         Ok((host_offset_start.0, allocated_length))
     }
+
+    /// Inner implementation for [`Qcow2::do_ensure_fixed_mapping()`].
+    ///
+    /// Does not do any clean-up: The L2 table will probably be modified, but not written to disk.
+    /// Any existing allocations that have been removed from it (and are thus leaked) are entered
+    /// into `leaked_allocations`, but not freed.
+    ///
+    /// The caller must do both, ensuring it is done both in case of success and in case of error.
+    ///
+    /// Allows zeroing or discarding clusters.  `mapping` says which kind of mapping to create.
+    async fn ensure_fixed_mapping_no_cleanup(
+        &self,
+        first_cluster: GuestCluster,
+        count: ClusterCount,
+        mapping: FixedMapping,
+        mut l2_table: L2TableWriteGuard<'_>,
+        leaked_allocations: &mut Vec<(HostCluster, ClusterCount)>,
+    ) -> io::Result<ClusterCount> {
+        self.header.require_version(3)?;
+
+        let cb = self.header.cluster_bits();
+        let mut cluster = first_cluster;
+        let end_cluster = first_cluster + count;
+
+        while cluster < end_cluster {
+            let l2i = cluster.l2_index(cb);
+            let leaked = match mapping {
+                FixedMapping::ZeroDiscard => l2_table.zero_cluster(l2i, false)?,
+                FixedMapping::ZeroRetainAllocation => l2_table.zero_cluster(l2i, true)?,
+                FixedMapping::FullDiscard => l2_table.discard_cluster(l2i),
+            };
+            if let Some(leaked) = leaked {
+                leaked_allocations.push(leaked);
+            }
+
+            let Some(next) = cluster.next_in_l2(cb) else {
+                break;
+            };
+            cluster = next;
+        }
+
+        Ok(cluster - first_cluster)
+    }
+}
+
+/// Possible mapping types for [`Qcow2::ensure_fixed_mapping()`].
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub(super) enum FixedMapping {
+    /// Make all clusters zero clusters, discarding previous allocations.
+    ///
+    /// Note this breaks existing mapping information, which must be communicated somehow, for
+    /// example by requiring mutable access to the `Qcow2` object.
+    ZeroDiscard,
+
+    /// Make all clusters zero clusters, retaining previous allocations.
+    ///
+    /// Retains previous data cluster allocations in the form of preallocated zero clusters, but
+    /// cannot retain previously existing compressed cluster allocations.  Because those mappings
+    /// are not returned through the mapping interface, however, concurrent accesses should be
+    /// reasonably safe.
+    ///
+    /// (Writing to zeroed data cluster mappings will just have no effect.)
+    ZeroRetainAllocation,
+
+    /// Fully remove clusters’ mappings, allowing backing data to appear.
+    ///
+    /// Note this breaks existing mapping information, which must be communicated somehow, for
+    /// example by requiring mutable access to the `Qcow2` object.
+    FullDiscard,
 }

src/format/access.rs
@@ -4,6 +4,7 @@
 
 use super::drivers::{self, FormatDriverInstance};
 use crate::io_buffers::{IoVector, IoVectorMut};
+use crate::storage::ext::write_full_zeroes;
 use crate::vector_select::FutureVector;
 use crate::{Storage, StorageExt};
 use std::fmt::{self, Display, Formatter};
@@ -359,6 +360,188 @@ impl<S: Storage> FormatAccess<S> {
     /// the end of file before the end of the buffer results in an error.
     pub async fn write(&self, buf: impl Into<IoVector<'_>>, offset: u64) -> io::Result<()> {
         self.writev(buf.into(), offset).await
+    }
+
+    /// Ensure the given range reads as zeroes, without write-zeroes support.
+    ///
+    /// Does not require support for efficient zeroing, instead writing zeroes when the range is
+    /// not zero yet.  If `allocate` is true, areas that are not currently allocated will be
+    /// allocated to write zeroes there; if it is false, unallocated areas that currently read as
+    /// zero are left alone.
+    async fn soft_ensure_zero(
+        &self,
+        mut offset: u64,
+        mut length: u64,
+        allocate: bool,
+    ) -> io::Result<()> {
+        while length > 0 {
+            let (mapping, mlen) = self.inner.get_mapping(offset, length).await?;
+            let mlen = cmp::min(mlen, length);
+
+            let mapping = match mapping {
+                drivers::Mapping::Raw {
+                    storage,
+                    offset,
+                    writable,
+                } => writable.then_some((storage, offset)),
+                drivers::Mapping::Indirect {
+                    layer: _,
+                    offset: _,
+                    writable: _,
+                } => None,
+                drivers::Mapping::Zero => {
+                    if allocate {
+                        None
+                    } else {
+                        offset += mlen;
+                        length -= mlen;
+                        continue;
+                    }
+                }
+                drivers::Mapping::Eof => {
+                    return Err(io::ErrorKind::UnexpectedEof.into());
+                }
+                drivers::Mapping::Special { offset: _ } => None,
+            };
+
+            let (file, mofs, mlen) = if let Some((file, mofs)) = mapping {
+                (file, mofs, mlen)
+            } else {
+                self.ensure_data_mapping(offset, length, true).await?
+            };
+
+            write_full_zeroes(file, mofs, mlen).await?;
+            offset += mlen;
+            length -= mlen;
+        }
+
+        Ok(())
+    }
+
+    /// Ensure the given range reads as zeroes.
+    ///
+    /// May use efficient zeroing for a subset of the given range, if supported by the format.
+    /// Will not discard anything, which keeps existing data mappings usable, albeit writing to
+    /// mappings that are now zeroed may have no effect.
+    ///
+    /// Check if [`FormatAccess::discard_to_zero()`] better suits your needs: It may work better on
+    /// a wider range of formats (`write_zeroes()` requires support for preallocated zero clusters,
+    /// which qcow2 does have, but other formats may not), and can actually free up space.
+    /// However, because it can break existing data mappings, it requires a mutable `self`
+    /// reference.
+    pub async fn write_zeroes(&self, mut offset: u64, length: u64) -> io::Result<()> {
+        let max_offset = offset.checked_add(length).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidInput, "Write-zeroes range overflow")
+        })?;
+
+        while offset < max_offset {
+            let (zofs, zlen) = self
+                .inner
+                .ensure_zero_mapping(offset, max_offset - offset)
+                .await?;
+            if zlen == 0 {
+                break;
+            }
+            // Fill up head, i.e. the range [offset, zofs)
+            self.soft_ensure_zero(offset, zofs - offset, true).await?;
+            offset = zofs + zlen;
+        }
+
+        // Fill up tail, i.e. the remaining range [offset, max_offset)
+        self.soft_ensure_zero(offset, max_offset - offset, true)
+            .await?;
+        Ok(())
+    }
+
+    /// Discard the given range, ensure it is read back as zeroes.
+    ///
+    /// Effectively the same as [`FormatAccess::write_zeroes()`], but discard as much of the
+    /// existing allocation as possible.  This breaks existing data mappings, so needs a mutable
+    /// reference to `self`, which ensures that existing data references (which have the lifetime
+    /// of an immutable `self` reference) cannot be kept.
+    ///
+    /// Areas that cannot be discarded (because of format-inherent alignment restrictions) are
+    /// still overwritten with zeroes, unless discarding is not supported altogether.
+    pub async fn discard_to_zero(&mut self, mut offset: u64, length: u64) -> io::Result<()> {
+        let max_offset = offset.checked_add(length).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Discard-to-zero range overflow",
+            )
+        })?;
+
+        while offset < max_offset {
+            let (zofs, zlen) = self
+                .inner
+                .discard_to_zero(offset, max_offset - offset)
+                .await?;
+            if zlen == 0 {
+                break;
+            }
+            // Fill up head, i.e. the range [offset, zofs)
+            self.soft_ensure_zero(offset, zofs - offset, false).await?;
+            offset = zofs + zlen;
+        }
+
+        // Fill up tail, i.e. the remaining range [offset, max_offset)
+        self.soft_ensure_zero(offset, max_offset - offset, false)
+            .await?;
+        Ok(())
+    }
+
+    /// Discard the given range, not guaranteeing specific data on read-back.
+    ///
+    /// Discard as much of the given range as possible, and keep the rest as-is.  Does not
+    /// guarantee any specific data on read-back, in contrast to
+    /// [`FormatAccess::discard_to_zero()`].
+    ///
+    /// Discarding being unsupported by this format is still returned as an error
+    /// ([`std::io::ErrorKind::Unsupported`])
+    pub async fn discard_to_any(&mut self, mut offset: u64, length: u64) -> io::Result<()> {
+        let max_offset = offset.checked_add(length).ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidInput, "Discard-to-any range overflow")
+        })?;
+
+        while offset < max_offset {
+            let (dofs, dlen) = self
+                .inner
+                .discard_to_any(offset, max_offset - offset)
+                .await?;
+            if dlen == 0 {
+                break;
+            }
+            offset = dofs + dlen;
+        }
+
+        Ok(())
+    }
+
+    /// Discard the given range, such that the backing image becomes visible.
+    ///
+    /// Discard as much of the given range as possible so that a backing image’s data becomes
+    /// visible, and keep the rest as-is.  This breaks existing data mappings, so needs a mutable
+    /// reference to `self`, which ensures that existing data references (which have the lifetime
+    /// of an immutable `self` reference) cannot be kept.
+    pub async fn discard_to_backing(&mut self, mut offset: u64, length: u64) -> io::Result<()> {
+        let max_offset = offset.checked_add(length).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "Discard-to-backing range overflow",
+            )
+        })?;
+
+        while offset < max_offset {
+            let (dofs, dlen) = self
+                .inner
+                .discard_to_backing(offset, max_offset - offset)
+                .await?;
+            if dlen == 0 {
+                break;
+            }
+            offset = dofs + dlen;
+        }
+
+        Ok(())
     }
 
     /// Flush internal buffers.  Always call this before drop!

src/format/drivers.rs
@@ -98,12 +98,67 @@ pub trait FormatDriverInstance: Debug + Display + Send + Sync {
     ///
     /// If `overwrite` is true, the contents in the range are supposed to be overwritten and may be
     /// discarded.  Otherwise, they must be kept.
+    ///
+    /// Should not break existing data mappings, i.e. not discard or repurpose existing data
+    /// mappings.  Making them unused, but retaining them as allocated so they can safely be
+    /// written to (albeit with no effect) is OK; discarding them so that they may be reused for
+    /// other mappings is not.
     async fn ensure_data_mapping<'a>(
         &'a self,
         offset: u64,
         length: u64,
         overwrite: bool,
     ) -> io::Result<(&'a Self::Storage, u64, u64)>;
+
+    /// Ensure that the given range is efficiently mapped as zeroes.
+    ///
+    /// Must not write any data.  Return the range (offset and length) that could actually be
+    /// zeroed, which must be a subset of the range given by `offset` and `length`.  The returned
+    /// offset must be as close to `offset` as possible, i.e. no zero mapping is possible between
+    /// `offset` and the returned offset (e.g. because of format-inherent granularity).
+    ///
+    /// The returned length may be zero in case zeroing would theoretically be possible, but not
+    /// for this range at this granularity.
+    ///
+    /// Should not break existing data mappings, i.e. not discard or repurpose existing data
+    /// mappings.  Making them unused, but retaining them as allocated so they can safely be
+    /// written to (albeit with no effect) is OK; discarding them so that they may be reused for
+    /// other mappings is not.
+    async fn ensure_zero_mapping(&self, _offset: u64, _length: u64) -> io::Result<(u64, u64)> {
+        Err(io::ErrorKind::Unsupported.into())
+    }
+
+    /// Discard the given range, ensure it is read back as zeroes.
+    ///
+    /// Effectively the same as [`FormatDriverInstance::ensure_zero_mapping()`], but may break
+    /// existing data mappings thanks to the mutable `self` reference, which ensures that old data
+    /// mappings returned by [`FormatDriverInstance::get_mapping()`] cannot be held onto.
+    async fn discard_to_zero(&mut self, _offset: u64, _length: u64) -> io::Result<(u64, u64)> {
+        Err(io::ErrorKind::Unsupported.into())
+    }
+
+    /// Discard the given range.
+    ///
+    /// Effectively the same as [`FormatDriverInstance::discard_to_zero()`], but the discarded area
+    /// may read as any data.  Backing file data should not reappear, however.
+    async fn discard_to_any(&mut self, _offset: u64, _length: u64) -> io::Result<(u64, u64)> {
+        Err(io::ErrorKind::Unsupported.into())
+    }
+
+    /// Discard the given range, such that the backing image becomes visible.
+    ///
+    /// Deallocate the range such that in deallocated blocks, the backing image’s data (if one
+    /// exists) will show, i.e. [`FormatDriverInstance::get_mapping()`] should return an indirect
+    /// mapping.  When there is no backing image, those blocks should appear as zero.
+    ///
+    /// Return the range (offset and length) that could actually be discarded, which must be a
+    /// subset of `offset` and `length`, and the returned offset must be as close to `offset` as
+    /// possible (like for [`FormatDriverInstance::discard_to_backing()`].
+    ///
+    /// May break existing data mappings thanks to the mutable `self` reference.
+    async fn discard_to_backing(&mut self, _offset: u64, _length: u64) -> io::Result<(u64, u64)> {
+        Err(io::ErrorKind::Unsupported.into())
+    }
 
     /// Read data from a `Mapping::Special` area.
     async fn readv_special(&self, _bufv: IoVectorMut<'_>, _offset: u64) -> io::Result<()> {

src/format/sync_wrappers.rs
@@ -139,6 +139,54 @@ impl<S: Storage> SyncFormatAccess<S> {
         self.writev(buf.into(), offset)
     }
 
+    /// Ensure the given range reads as zeroes.
+    ///
+    /// May use efficient zeroing for a subset of the given range, if supported by the format.
+    /// Will not discard anything, which keeps existing data mappings usable, albeit writing to
+    /// mappings that are now zeroed may have no effect.
+    pub fn write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.write_zeroes(offset, length))
+    }
+
+    /// Discard the given range, ensure it is read back as zeroes.
+    ///
+    /// Effectively the same as [`FormatAccess::write_zeroes()`], but discard as much of the
+    /// existing allocation as possible.  This breaks existing data mappings, so needs a mutable
+    /// reference to `self`, which ensures that existing data references (which have the lifetime
+    /// of an immutable `self` reference) cannot be kept.
+    ///
+    /// Areas that cannot be discarded (because of format-inherent alignment restrictions) are
+    /// still overwritten with zeroes, unless discarding is not supported altogether.
+    pub fn discard_to_zero(&mut self, offset: u64, length: u64) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.discard_to_zero(offset, length))
+    }
+
+    /// Discard the given range, not guaranteeing specific data on read-back.
+    ///
+    /// Discard as much of the given range as possible, and keep the rest as-is.  Does not
+    /// guarantee any specific data on read-back, in contrast to
+    /// [`FormatAccess::discard_to_zero()`].
+    ///
+    /// Discarding being unsupported by this format is still returned as an error
+    /// ([`std::io::ErrorKind::Unsupported`])
+    pub fn discard_to_any(&mut self, offset: u64, length: u64) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.discard_to_any(offset, length))
+    }
+
+    /// Discard the given range, such that the backing image becomes visible.
+    ///
+    /// Discard as much of the given range as possible so that a backing image’s data becomes
+    /// visible, and keep the rest as-is.  This breaks existing data mappings, so needs a mutable
+    /// reference to `self`, which ensures that existing data references (which have the lifetime
+    /// of an immutable `self` reference) cannot be kept.
+    pub fn discard_to_backing(&mut self, offset: u64, length: u64) -> io::Result<()> {
+        self.runtime
+            .block_on(self.inner.discard_to_backing(offset, length))
+    }
+
     /// Flush internal buffers.
     ///
     /// Does not necessarily sync those buffers to disk.  When using `flush()`, consider whether

src/qcow2/metadata.rs
@@ -643,6 +643,21 @@ impl Header {
         self.v2.size
     }
 
+    /// Require a minimum qcow2 version.
+    ///
+    /// Return an error if the version requirement is not met.
+    pub fn require_version(&self, minimum: u32) -> io::Result<()> {
+        let version = self.v2.version;
+        if version >= minimum {
+            Ok(())
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::Unsupported,
+                format!("qcow2 version {minimum} required, image has version {version}"),
+            ))
+        }
+    }
+
     /// log2 of the cluster size.
     pub fn cluster_bits(&self) -> u32 {
         self.v2.cluster_bits
@@ -1626,7 +1641,7 @@ impl L2TableWriteGuard<'_> {
     ///
     /// If the allocation is reused, `None` is returned, so this function only returns `Some(_)` if
     /// some cluster is indeed leaked.
-    #[must_use]
+    #[must_use = "Leaked allocation must be freed"]
     pub fn map_cluster(
         &mut self,
         index: usize,
@@ -1654,6 +1669,93 @@ impl L2TableWriteGuard<'_> {
         } else {
             None
         }
+    }
+
+    /// Make the given index a zero mapping.
+    ///
+    /// If `keep_allocation` is true, keep the zero cluster pre-allocated if there is a
+    /// pre-existing single-cluster allocation (i.e. data cluster or pre-allocated zero cluster).
+    /// Otherwise, the existing mapping is discarded.
+    ///
+    /// If a previous mapping is discarded, return the old allocation so its refcount can be
+    /// decreased (offset of the first cluster and number of clusters -- compressed clusters can
+    /// span across host cluster boundaries).
+    #[must_use = "Leaked allocation must be freed"]
+    pub fn zero_cluster(
+        &mut self,
+        index: usize,
+        keep_allocation: bool,
+    ) -> io::Result<Option<(HostCluster, ClusterCount)>> {
+        let cluster_copied = if keep_allocation {
+            match self.table.data[index].get().into_mapping(
+                GuestCluster(0), // only used for backing, which we ignore
+                self.table.cluster_bits,
+                self.table.external_data_file,
+            )? {
+                L2Mapping::DataFile {
+                    host_cluster,
+                    copied,
+                } => Some((host_cluster, copied)),
+                L2Mapping::Backing { backing_offset: _ } => None,
+                L2Mapping::Zero {
+                    host_cluster: Some(host_cluster),
+                    copied,
+                } => Some((host_cluster, copied)),
+                L2Mapping::Zero {
+                    host_cluster: None,
+                    copied: _,
+                } => None,
+                L2Mapping::Compressed {
+                    host_offset: _,
+                    length: _,
+                } => None,
+            }
+        } else {
+            None
+        };
+
+        let retained = cluster_copied.is_some();
+        let new = if let Some((cluster, copied)) = cluster_copied {
+            L2Mapping::Zero {
+                host_cluster: Some(cluster),
+                copied,
+            }
+        } else {
+            L2Mapping::Zero {
+                host_cluster: None,
+                copied: false,
+            }
+        };
+        let new = L2Entry::from_mapping(new, self.table.cluster_bits);
+
+        // Safe: We set a full valid mapping, and there is only one writer (thanks to
+        // `L2TableWriteGuard`).
+        let old = unsafe { self.table.data[index].swap(new) };
+        self.table.modified.store(true, Ordering::Relaxed);
+
+        let leaked = if !retained {
+            old.allocation(self.table.cluster_bits, self.table.external_data_file)
+        } else {
+            None
+        };
+        Ok(leaked)
+    }
+
+    /// Remove the given mapping, leaving it empty.
+    ///
+    /// If a previous mapping is discarded, return the old allocation so its refcount can be
+    /// decreased (offset of the first cluster and number of clusters -- compressed clusters can
+    /// span across host cluster boundaries).
+    #[must_use = "Leaked allocation must be freed"]
+    pub fn discard_cluster(&mut self, index: usize) -> Option<(HostCluster, ClusterCount)> {
+        let new = L2Entry(0);
+
+        // Safe: We set a full valid mapping, and there is only one writer (thanks to
+        // `L2TableWriteGuard`).
+        let old = unsafe { self.table.data[index].swap(new) };
+        self.table.modified.store(true, Ordering::Relaxed);
+
+        old.allocation(self.table.cluster_bits, self.table.external_data_file)
     }
 }
 

src/qcow2/mod.rs
@@ -26,6 +26,7 @@ use allocation::Allocator;
 use async_trait::async_trait;
 pub use builder::Qcow2OpenBuilder;
 use cache::L2CacheBackend;
+use mappings::FixedMapping;
 use metadata::*;
 use std::fmt::{self, Debug, Display, Formatter};
 use std::ops::Range;
@@ -476,6 +477,54 @@ impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
         self.need_writable()?;
         let offset = GuestOffset(offset);
         self.do_ensure_data_mapping(offset, length, overwrite).await
+    }
+
+    async fn ensure_zero_mapping(&self, offset: u64, length: u64) -> io::Result<(u64, u64)> {
+        self.need_writable()?;
+        self.check_disk_bounds(offset, length, "write")?;
+
+        let (first_cluster, count) = self
+            .ensure_fixed_mapping(
+                GuestOffset(offset),
+                length,
+                FixedMapping::ZeroRetainAllocation,
+            )
+            .await?;
+
+        let cb = self.header.cluster_bits();
+        Ok((first_cluster.offset(cb).0, count.byte_size(cb)))
+    }
+
+    async fn discard_to_zero(&mut self, offset: u64, length: u64) -> io::Result<(u64, u64)> {
+        self.need_writable()?;
+        self.check_disk_bounds(offset, length, "discard")?;
+
+        // Safe to discard: We have a mutable `self` reference
+        // Note this will return an `Unsupported` error for v2 images.  That’s OK, safely
+        // discarding on them is a hairy affair, and they are really outdated by now.
+        let (first_cluster, count) = self
+            .ensure_fixed_mapping(GuestOffset(offset), length, FixedMapping::ZeroDiscard)
+            .await?;
+
+        let cb = self.header.cluster_bits();
+        Ok((first_cluster.offset(cb).0, count.byte_size(cb)))
+    }
+
+    async fn discard_to_any(&mut self, offset: u64, length: u64) -> io::Result<(u64, u64)> {
+        self.discard_to_zero(offset, length).await
+    }
+
+    async fn discard_to_backing(&mut self, offset: u64, length: u64) -> io::Result<(u64, u64)> {
+        self.need_writable()?;
+        self.check_disk_bounds(offset, length, "discard")?;
+
+        // Safe to discard: We have a mutable `self` reference
+        let (first_cluster, count) = self
+            .ensure_fixed_mapping(GuestOffset(offset), length, FixedMapping::FullDiscard)
+            .await?;
+
+        let cb = self.header.cluster_bits();
+        Ok((first_cluster.offset(cb).0, count.byte_size(cb)))
     }
 
     async fn readv_special(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {

commit 157041ddf02d42304968cdfa4b0986d2a29e3962

qcow2: Fix reported discard length
Off-by-one error: In case we hit the end of an L2 table, we reported
that we discarded on cluster less than we actually did.  With the caller
then probably retrying, we would next report 0 clusters (instead of 1),
leading the caller to stop trying altogether.

Fixes: 0ea71defd1c5d423d41a26ef22e0b63116e60ffa
       (“Formats: Add discarding and write-zeroes”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mappings.rs
@@ -445,6 +445,7 @@ impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
         let cb = self.header.cluster_bits();
         let mut cluster = first_cluster;
         let end_cluster = first_cluster + count;
+        let mut done = ClusterCount(0);
 
         while cluster < end_cluster {
             let l2i = cluster.l2_index(cb);
@@ -457,13 +458,14 @@ impl<S: Storage, F: WrappedFormat<S>> Qcow2<S, F> {
                 leaked_allocations.push(leaked);
             }
 
+            done += ClusterCount(1);
             let Some(next) = cluster.next_in_l2(cb) else {
                 break;
             };
             cluster = next;
         }
 
-        Ok(cluster - first_cluster)
+        Ok(done)
     }
 }
 