XanClic/imago

commit 7c6459ef1307b3fc3dacc8ab5b8f6503b36a1d66

Pull StorageWrapper into Storage objects
Effectively rename `StorageWrapper` to `CommonStorageHelper`, and have
`Storage` objects store this object.  Move all methods from
`StorageWrapper` back into the `StorageExt` trait.

This effectively reverts 15d2157920707373451c26e3348f0df666d34da3 (“Wrap
Storage objects in StorageWrapper”).

The problem was: Users create `Storage` objects.  They may access those
directly, bypassing the `StorageWrapper`, and thus bypassing mandatory
write serialization.  The same may happen accidentally, when using the
same `Storage` object instance for two different format drivers; those
would not see each other’s `StorageWrapper`.  Therefore, the
serialization must happen directly in the `Storage` object (via the
`StorageExt` trait).

Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -16,7 +16,7 @@ use crate::format::drivers::{FormatDriverInstance, Mapping};
 use crate::format::wrapped::WrappedFormat;
 use crate::io_buffers::{IoVectorMut, IoVectorTrait};
 use crate::raw::Raw;
-use crate::{FormatAccess, Storage, StorageOpenOptions, StorageWrapper};
+use crate::{FormatAccess, Storage, StorageExt, StorageOpenOptions};
 use allocation::Allocator;
 use async_trait::async_trait;
 use cache::L2CacheBackend;
@@ -37,7 +37,7 @@ use types::*;
 /// - Backing image `WrappedFormat<S>`: A backing disk image in any format
 pub struct Qcow2<S: Storage + 'static, F: WrappedFormat<S> + 'static = FormatAccess<S>> {
     /// Image file (which contains the qcow2 metadata).
-    metadata: Arc<StorageWrapper<S>>,
+    metadata: Arc<S>,
 
     /// Whether this image may be modified.
     writable: bool,
@@ -45,7 +45,7 @@ pub struct Qcow2<S: Storage + 'static, F: WrappedFormat<S> + 'static = FormatAcc
     /// Whether the user explicitly assigned a data file storage object (or `None`).
     storage_set: bool,
     /// Data file storage object; will use `metadata` if `None`.
-    storage: Option<StorageWrapper<S>>,
+    storage: Option<S>,
     /// Whether the user explicitly assigned a backing file (or `None`).
     backing_set: bool,
     /// Backing image.
@@ -79,8 +79,6 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
     /// If you want to use the implicit references given in the image header, use
     /// [`Qcow2::open_implicit_dependencies()`].
     pub async fn open_image(metadata: S, writable: bool) -> io::Result<Self> {
-        let metadata = Arc::new(metadata.into());
-
         let header = Arc::new(Header::load(&metadata, writable).await?);
 
         let cb = header.cluster_bits();
@@ -94,6 +92,8 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
 
         let l1_table =
             L1Table::load(&metadata, &header, l1_cluster, header.l1_table_entries()).await?;
+
+        let metadata = Arc::new(metadata);
 
         let allocator = if writable {
             let allocator = Allocator::new(Arc::clone(&metadata), Arc::clone(&header)).await?;
@@ -143,8 +143,7 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
 
     /// Check whether the given image file is a qcow2 file.
     pub(crate) async fn probe(metadata: &S) -> io::Result<()> {
-        let metadata = metadata.into();
-        Header::load(&metadata, true).await?;
+        Header::load(metadata, true).await?;
         Ok(())
     }
 
@@ -200,7 +199,7 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
     ///
     /// If we have an external data file, return that.  Otherwise, return the image (metadata)
     /// file.
-    fn storage(&self) -> &'_ StorageWrapper<S> {
+    fn storage(&self) -> &'_ S {
         self.storage.as_ref().unwrap_or(&self.metadata)
     }
 
@@ -307,7 +306,7 @@ impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
         self.header.size()
     }
 
-    fn collect_storage_dependencies(&self) -> Vec<&'_ StorageWrapper<S>> {
+    fn collect_storage_dependencies(&self) -> Vec<&'_ S> {
         let mut v = self
             .backing
             .as_ref()
@@ -342,7 +341,7 @@ impl<S: Storage, F: WrappedFormat<S>> FormatDriverInstance for Qcow2<S, F> {
         offset: u64,
         length: u64,
         overwrite: bool,
-    ) -> io::Result<(&'_ StorageWrapper<S>, u64, u64)> {
+    ) -> io::Result<(&'_ S, u64, u64)> {
         let length_until_eof = self.header.size().saturating_sub(offset);
         if length_until_eof < length {
             return Err(io::Error::other("Cannot allocate beyond the disk size"));

src/storage/mod.rs
@@ -1,0 +1,449 @@
+//! Helper functionality to access storage.
+//!
+//! While not the primary purpose of this crate, to open VM images, we need to be able to access
+//! different kinds of storage objects.  Such objects are abstracted behind the `Storage` trait.
+
+pub(crate) mod drivers;
+pub mod ext;
+
+use crate::io_buffers::{IoBuffer, IoBufferRefTrait, IoVector, IoVectorMut};
+use drivers::CommonStorageHelper;
+use ext::StorageExt;
+use std::fmt::{Debug, Display};
+use std::future::Future;
+use std::path::{Path, PathBuf};
+use std::pin::Pin;
+use std::sync::Arc;
+use std::{cmp, io};
+
+/// Parameters from which a storage object can be constructed.
+#[derive(Clone, Default)]
+pub struct StorageOpenOptions {
+    /// Filename to open.
+    pub(crate) filename: Option<PathBuf>,
+
+    /// Whether the object should be opened as writable or read-only.
+    pub(crate) writable: bool,
+
+    /// Whether to bypass the host page cache (if applicable).
+    pub(crate) direct: bool,
+}
+
+/// Implementation for storage objects.
+pub trait Storage: Debug + Display + Send + Sized + Sync {
+    /// Open a storage object.
+    ///
+    /// Different storage implementations may require different options.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async fn open(_opts: StorageOpenOptions) -> io::Result<Self> {
+        Err(io::Error::new(
+            io::ErrorKind::Unsupported,
+            format!(
+                "Cannot open storage objects of type {}",
+                std::any::type_name::<Self>()
+            ),
+        ))
+    }
+
+    /// Minimum required alignment for memory buffers.
+    fn mem_align(&self) -> usize {
+        1
+    }
+
+    /// Minimum required alignment for offsets and lengths.
+    fn req_align(&self) -> usize {
+        1
+    }
+
+    /// Storage object length.
+    fn size(&self) -> io::Result<u64>;
+
+    /// Read data at `offset` into `bufv`.
+    ///
+    /// Reads until `bufv` is filled completely, i.e. will not do short reads.  When reaching the
+    /// end of file, the rest of `bufv` is filled with 0.
+    ///
+    /// # Safety
+    /// This is a pure read from storage.  The request must be fully aligned to
+    /// [`Self::mem_align()`] and [`Self::req_align()`], and safeguards we want to implement for
+    /// safe concurrent access may not be available.
+    ///
+    /// Use [`StorageExt::readv()`] instead.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()>;
+
+    /// Write data from `bufv` to `offset`.
+    ///
+    /// Writes all data from `bufv`, i.e. will not do short writes.  When reaching the end of file,
+    /// grow it as necessary so that the new end of file will be at `offset + bufv.len()`.
+    ///
+    /// If growing is not possible, writes beyond the end of file (even if only partially) should
+    /// fail.
+    ///
+    /// # Safety
+    /// This is a pure write to storage.  The request must be fully aligned to
+    /// [`Self::mem_align()`] and [`Self::req_align()`], and safeguards we want to implement for
+    /// safe concurrent access may not be available.
+    ///
+    /// Use [`StorageExt::writev()`] instead.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()>;
+
+    /// Ensure the given range reads back as zeroes.
+    ///
+    /// The default implementation writes actual zeroes as data, which is inefficient.  Storage
+    /// drivers should override it with a more efficient implementation.
+    ///
+    /// # Safety
+    /// This is a pure write to storage.  The request must be fully aligned to
+    /// [`Self::req_align()`], and safeguards we want to implement for safe concurrent access may
+    /// not be available.
+    ///
+    /// Use [`StorageExt::write_zeroes()`] instead.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async unsafe fn pure_write_zeroes(&self, mut offset: u64, mut length: u64) -> io::Result<()> {
+        let buflen = cmp::min(length, 1048576) as usize;
+        let mut buf = IoBuffer::new(buflen, self.mem_align())?;
+        buf.as_mut().into_slice().fill(0);
+
+        while length > 0 {
+            let chunk_length = cmp::min(length, 1048576) as usize;
+            self.writev(buf.as_ref_range(0..chunk_length).into(), offset)
+                .await?;
+            offset += chunk_length as u64;
+            length -= chunk_length as u64;
+        }
+
+        Ok(())
+    }
+
+    /// Discard the given range, with undefined contents when read back.
+    ///
+    /// Tell the storage layer this range is no longer needed and need not be backed by actual
+    /// storage.  When read back, the data read will be undefined, i.e. not necessarily zeroes.
+    ///
+    /// No-op implementations therefore explicitly fulfill the interface contract.
+    ///
+    /// # Safety
+    /// This is a pure write to storage.  Safeguards we want to implement for safe concurrent
+    /// access may not be available.
+    ///
+    /// Use [`StorageExt::discard()`] instead.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async unsafe fn pure_discard(&self, _offset: u64, _length: u64) -> io::Result<()> {
+        Ok(())
+    }
+
+    /// Flush internal buffers.
+    ///
+    /// Does not necessarily sync those buffers to disk.  When using `flush()`, consider whether
+    /// you want to call `sync()` afterwards.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async fn flush(&self) -> io::Result<()>;
+
+    /// Sync data already written to the storage hardware.
+    ///
+    /// This does not necessarily include flushing internal buffers, i.e. `flush`.  When using
+    /// `sync()`, consider whether you want to call `flush()` before it.
+    #[allow(async_fn_in_trait)] // No need for Send
+    async fn sync(&self) -> io::Result<()>;
+
+    /// Return the storage helper object (used by the [`StorageExt`] implementation).
+    fn get_storage_helper(&self) -> &CommonStorageHelper;
+}
+
+/// Allow dynamic use of storage objects (i.e. is object safe).
+///
+/// When using normal `Storage` objects, they must all be of the same type within a single disk
+/// image chain.  For example, every storage object underneath a `FormatAccess<StdFile>` object
+/// must be of type `StdFile`.
+///
+/// `DynStorage` allows the use of `Box<dyn DynStorage>`, which implements `Storage`, to allow
+/// mixed storage object types.  Therefore, a `FormatAccess<Box<dyn DynStorage>>` allows e.g. the
+/// use of both `Box<StdFile>` and `Box<Null>` storage objects together.  (`Arc` instead of `Box`
+/// works, too.)
+///
+/// Async functions in `DynStorage` return boxed futures (`Pin<Box<dyn Future>>`), which makes them
+/// slighly less efficient than async functions in `Storage`, hence the distinction.
+pub trait DynStorage: Debug + Display + Send + Sync {
+    /// Wrapper around [`Storage::mem_align()`].
+    fn mem_align(&self) -> usize;
+
+    /// Wrapper around [`Storage::req_align()`].
+    fn req_align(&self) -> usize;
+
+    /// Wrapper around [`Storage::size()`].
+    fn size(&self) -> io::Result<u64>;
+
+    /// Object-safe wrapper around [`Storage::pure_readv()`].
+    ///
+    /// # Safety
+    /// Same considerations are for [`Storage::pure_readv()`] apply.
+    unsafe fn pure_readv<'a>(
+        &'a self,
+        bufv: IoVectorMut<'a>,
+        offset: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + 'a>>;
+
+    /// Object-safe wrapper around [`Storage::pure_writev()`].
+    ///
+    /// # Safety
+    /// Same considerations are for [`Storage::pure_writev()`] apply.
+    unsafe fn pure_writev<'a>(
+        &'a self,
+        bufv: IoVector<'a>,
+        offset: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + 'a>>;
+
+    /// Object-safe wrapper around [`Storage::pure_write_zeroes()`].
+    ///
+    /// # Safety
+    /// Same considerations are for [`Storage::pure_write_zeroes()`] apply.
+    unsafe fn pure_write_zeroes(
+        &self,
+        offset: u64,
+        length: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>>;
+
+    /// Object-safe wrapper around [`Storage::pure_discard()`].
+    ///
+    /// # Safety
+    /// Same considerations are for [`Storage::pure_discard()`] apply.
+    unsafe fn pure_discard(
+        &self,
+        offset: u64,
+        length: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>>;
+
+    /// Object-safe wrapper around [`Storage::flush()`].
+    fn flush(&self) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>>;
+
+    /// Object-safe wrapper around [`Storage::sync()`].
+    fn sync(&self) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>>;
+
+    /// Wrapper around [`Storage::get_storage_helper()`].
+    fn get_storage_helper(&self) -> &CommonStorageHelper;
+}
+
+impl<S: Storage> Storage for &S {
+    fn mem_align(&self) -> usize {
+        (*self).mem_align()
+    }
+
+    fn req_align(&self) -> usize {
+        (*self).req_align()
+    }
+
+    fn size(&self) -> io::Result<u64> {
+        (*self).size()
+    }
+
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {
+        unsafe { (*self).pure_readv(bufv, offset).await }
+    }
+
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()> {
+        unsafe { (*self).pure_writev(bufv, offset).await }
+    }
+
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { (*self).pure_write_zeroes(offset, length).await }
+    }
+
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { (*self).pure_discard(offset, length).await }
+    }
+
+    async fn flush(&self) -> io::Result<()> {
+        (*self).flush().await
+    }
+
+    async fn sync(&self) -> io::Result<()> {
+        (*self).sync().await
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        (*self).get_storage_helper()
+    }
+}
+
+impl<S: Storage> DynStorage for S {
+    fn mem_align(&self) -> usize {
+        S::mem_align(self)
+    }
+
+    fn req_align(&self) -> usize {
+        S::req_align(self)
+    }
+
+    fn size(&self) -> io::Result<u64> {
+        S::size(self)
+    }
+
+    unsafe fn pure_readv<'a>(
+        &'a self,
+        bufv: IoVectorMut<'a>,
+        offset: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + 'a>> {
+        Box::pin(unsafe { S::pure_readv(self, bufv, offset) })
+    }
+
+    unsafe fn pure_writev<'a>(
+        &'a self,
+        bufv: IoVector<'a>,
+        offset: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + 'a>> {
+        Box::pin(unsafe { S::pure_writev(self, bufv, offset) })
+    }
+
+    unsafe fn pure_write_zeroes(
+        &self,
+        offset: u64,
+        length: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>> {
+        Box::pin(unsafe { S::pure_write_zeroes(self, offset, length) })
+    }
+
+    unsafe fn pure_discard(
+        &self,
+        offset: u64,
+        length: u64,
+    ) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>> {
+        Box::pin(unsafe { S::pure_discard(self, offset, length) })
+    }
+
+    fn flush(&self) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>> {
+        Box::pin(S::flush(self))
+    }
+
+    fn sync(&self) -> Pin<Box<dyn Future<Output = io::Result<()>> + '_>> {
+        Box::pin(S::sync(self))
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        S::get_storage_helper(self)
+    }
+}
+
+impl Storage for Box<dyn DynStorage> {
+    async fn open(_opts: StorageOpenOptions) -> io::Result<Self> {
+        Err(io::Error::new(
+            io::ErrorKind::Unsupported,
+            "Cannot open dynamic storage instances",
+        ))
+    }
+
+    fn mem_align(&self) -> usize {
+        <Self as DynStorage>::mem_align(self)
+    }
+
+    fn req_align(&self) -> usize {
+        <Self as DynStorage>::req_align(self)
+    }
+
+    fn size(&self) -> io::Result<u64> {
+        <Self as DynStorage>::size(self)
+    }
+
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_readv(self, bufv, offset).await }
+    }
+
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_writev(self, bufv, offset).await }
+    }
+
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_write_zeroes(self, offset, length).await }
+    }
+
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_discard(self, offset, length).await }
+    }
+
+    async fn flush(&self) -> io::Result<()> {
+        <Self as DynStorage>::flush(self).await
+    }
+
+    async fn sync(&self) -> io::Result<()> {
+        <Self as DynStorage>::sync(self).await
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        <Self as DynStorage>::get_storage_helper(self)
+    }
+}
+
+impl Storage for Arc<dyn DynStorage> {
+    async fn open(_opts: StorageOpenOptions) -> io::Result<Self> {
+        Err(io::Error::new(
+            io::ErrorKind::Unsupported,
+            "Cannot open dynamic storage instances",
+        ))
+    }
+
+    fn mem_align(&self) -> usize {
+        <Self as DynStorage>::mem_align(self)
+    }
+
+    fn req_align(&self) -> usize {
+        <Self as DynStorage>::req_align(self)
+    }
+
+    fn size(&self) -> io::Result<u64> {
+        <Self as DynStorage>::size(self)
+    }
+
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_readv(self, bufv, offset) }.await
+    }
+
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_writev(self, bufv, offset) }.await
+    }
+
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_write_zeroes(self, offset, length) }.await
+    }
+
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
+        unsafe { <Self as DynStorage>::pure_discard(self, offset, length) }.await
+    }
+
+    async fn flush(&self) -> io::Result<()> {
+        <Self as DynStorage>::flush(self).await
+    }
+
+    async fn sync(&self) -> io::Result<()> {
+        <Self as DynStorage>::sync(self).await
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        <Self as DynStorage>::get_storage_helper(self)
+    }
+}
+
+impl StorageOpenOptions {
+    /// Create default options.
+    pub fn new() -> Self {
+        StorageOpenOptions::default()
+    }
+
+    /// Set a filename to open.
+    pub fn filename<P: AsRef<Path>>(mut self, filename: P) -> Self {
+        self.filename = Some(filename.as_ref().to_owned());
+        self
+    }
+
+    /// Whether the storage should be writable or not.
+    pub fn write(mut self, write: bool) -> Self {
+        self.writable = write;
+        self
+    }
+
+    /// Whether to bypass the host page cache (if applicable).
+    pub fn direct(mut self, direct: bool) -> Self {
+        self.direct = direct;
+        self
+    }
+}

src/annotated.rs
@@ -42,6 +42,7 @@
 //! ```
 
 use crate::io_buffers::{IoVector, IoVectorMut};
+use crate::storage::drivers::CommonStorageHelper;
 use crate::{Storage, StorageOpenOptions};
 use std::fmt::{self, Debug, Display, Formatter};
 use std::io;
@@ -105,16 +106,24 @@ impl<T: Debug + Default + Display + Send + Sync, S: Storage> Storage for Annotat
         self.inner.size()
     }
 
-    async fn readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {
-        self.inner.readv(bufv, offset).await
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, offset: u64) -> io::Result<()> {
+        // Caller guarantees safety
+        unsafe { self.inner.pure_readv(bufv, offset) }.await
     }
 
-    async fn writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()> {
-        self.inner.writev(bufv, offset).await
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, offset: u64) -> io::Result<()> {
+        // Caller guarantees safety
+        unsafe { self.inner.pure_writev(bufv, offset) }.await
     }
 
-    async fn write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
-        self.inner.write_zeroes(offset, length).await
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        // Caller guarantees safety
+        unsafe { self.inner.pure_write_zeroes(offset, length) }.await
+    }
+
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
+        // Caller guarantees safety
+        unsafe { self.inner.pure_discard(offset, length) }.await
     }
 
     async fn flush(&self) -> io::Result<()> {
@@ -123,6 +132,11 @@ impl<T: Debug + Default + Display + Send + Sync, S: Storage> Storage for Annotat
 
     async fn sync(&self) -> io::Result<()> {
         self.inner.sync().await
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        // Share storage helper from inner (to e.g. get same request serialization)
+        self.inner.get_storage_helper()
     }
 }
 

src/file.rs
@@ -1,6 +1,7 @@
 //! Use a plain as storage.
 
 use crate::io_buffers::{IoVector, IoVectorMut, IoVectorTrait};
+use crate::storage::drivers::CommonStorageHelper;
 use crate::{Storage, StorageOpenOptions};
 use std::fmt::{self, Display, Formatter};
 use std::fs;
@@ -39,6 +40,9 @@ pub struct File {
     ///
     /// Third parties changing the length concurrently is pretty certain to break things anyway.
     size: AtomicU64,
+
+    /// Storage helper.
+    common_storage_helper: CommonStorageHelper,
 }
 
 impl TryFrom<fs::File> for File {
@@ -53,6 +57,7 @@ impl TryFrom<fs::File> for File {
             direct_io: false,
             filename: PathBuf::new(),
             size: AtomicU64::new(size),
+            common_storage_helper: Default::default(),
         })
     }
 }
@@ -98,6 +103,7 @@ impl Storage for File {
             direct_io: opts.direct,
             filename: filename_owned,
             size: AtomicU64::new(size),
+            common_storage_helper: Default::default(),
         })
     }
 
@@ -124,7 +130,7 @@ impl Storage for File {
     }
 
     #[cfg(unix)]
-    async fn readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
         let sz = self.size.load(Ordering::Relaxed);
         let end = offset
             .checked_add(bufv.len())
@@ -156,7 +162,7 @@ impl Storage for File {
     }
 
     #[cfg(windows)]
-    async fn readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
+    async unsafe fn pure_readv(&self, bufv: IoVectorMut<'_>, mut offset: u64) -> io::Result<()> {
         for mut buffer in bufv.into_inner() {
             let mut buffer: &mut [u8] = &mut buffer;
             while !buffer.is_empty() {
@@ -176,7 +182,7 @@ impl Storage for File {
     }
 
     #[cfg(unix)]
-    async fn writev(&self, bufv: IoVector<'_>, mut offset: u64) -> io::Result<()> {
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, mut offset: u64) -> io::Result<()> {
         // TODO: Use `write_vectored_at()` once `unix_file_vectored_at` is stable
         for buffer in bufv.into_inner() {
             let next_offset = offset
@@ -190,7 +196,7 @@ impl Storage for File {
     }
 
     #[cfg(windows)]
-    async fn writev(&self, bufv: IoVector<'_>, mut offset: u64) -> io::Result<()> {
+    async unsafe fn pure_writev(&self, bufv: IoVector<'_>, mut offset: u64) -> io::Result<()> {
         for buffer in bufv.into_inner() {
             let mut buffer: &[u8] = &buffer;
             while !buffer.is_empty() {
@@ -205,8 +211,17 @@ impl Storage for File {
         Ok(())
     }
 
+    #[cfg(any(target_os = "linux", windows, target_os = "macos"))]
+    async unsafe fn pure_write_zeroes(&self, offset: u64, length: u64) -> io::Result<()> {
+        // All of our discard methods also ensure the range reads back as zeroes
+        unsafe { self.pure_discard(offset, length) }.await
+    }
+
+    // Beware when adding new discard methods: This is called by `pure_write_zeroes()`, so the
+    // current expectation is that discarded ranges will read back as zeroes.  If the new method
+    // does not guarantee that, you will need to modify `pure_write_zeroes()`.
     #[cfg(target_os = "linux")]
-    async fn discard(&self, offset: u64, length: u64) -> io::Result<()> {
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
         if self.try_discard_by_truncate(offset, length)? {
             return Ok(());
         }
@@ -236,8 +251,11 @@ impl Storage for File {
         Ok(())
     }
 
+    // Beware when adding new discard methods: This is called by `pure_write_zeroes()`, so the
+    // current expectation is that discarded ranges will read back as zeroes.  If the new method
+    // does not guarantee that, you will need to modify `pure_write_zeroes()`.
     #[cfg(windows)]
-    async fn discard(&self, offset: u64, length: u64) -> io::Result<()> {
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
         if self.try_discard_by_truncate(offset, length)? {
             return Ok(());
         }
@@ -279,8 +297,11 @@ impl Storage for File {
         Ok(())
     }
 
+    // Beware when adding new discard methods: This is called by `pure_write_zeroes()`, so the
+    // current expectation is that discarded ranges will read back as zeroes.  If the new method
+    // does not guarantee that, you will need to modify `pure_write_zeroes()`.
     #[cfg(target_os = "macos")]
-    async fn discard(&self, offset: u64, length: u64) -> io::Result<()> {
+    async unsafe fn pure_discard(&self, offset: u64, length: u64) -> io::Result<()> {
         if self.try_discard_by_truncate(offset, length)? {
             return Ok(());
         }
@@ -315,6 +336,10 @@ impl Storage for File {
 
     async fn sync(&self) -> io::Result<()> {
         self.file.write().unwrap().sync_all()
+    }
+
+    fn get_storage_helper(&self) -> &CommonStorageHelper {
+        &self.common_storage_helper
     }
 }
 

commit 257993956ce106983c9618ee8c677e8c613932ed

Fix clippy warning: Identity into()
When removing the `StorageWrapper`, I forgot to remove the now-useless
conversion from these places (which used to convert from `S` into`
StorageWrapper<S>`).

Fixes: 7c6459ef1307b3fc3dacc8ab5b8f6503b36a1d66
       (“Pull StorageWrapper into Storage objects”)
Signed-off-by: Hanna Czenczek <hreitz@redhat.com>

src/qcow2/mod.rs
@@ -212,7 +212,7 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
     /// `None` means using the same data storage for both metadata and data, which should be used
     /// if [`Qcow2::requires_external_data_file()`] is `false`.
     pub fn set_data_file(&mut self, file: Option<S>) {
-        self.storage = file.map(Into::into);
+        self.storage = file;
         self.storage_set = true;
     }
 
@@ -359,10 +359,7 @@ impl<S: Storage + 'static, F: WrappedFormat<S> + 'static> Qcow2<S, F> {
         mut gate: G,
     ) -> io::Result<()> {
         if !self.storage_set {
-            self.storage = self
-                .open_implicit_data_file(&mut gate)
-                .await?
-                .map(Into::into);
+            self.storage = self.open_implicit_data_file(&mut gate).await?;
             self.storage_set = true;
         }
 